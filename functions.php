84e648a07e92ca5156a600b7dcf165eb71aa738b
craftwerk <pst@crftwrk.de>
2021-08-14 09:40:16 +0200

    pluggable function bootscore_pagination

--- functions.php
+++ functions.php
@@ -321,50 +321,54 @@ endif;
 
 
 // Pagination Categories
-function bootscore_pagination($pages = '', $range = 2) 
-{  
-	$showitems = ($range * 2) + 1;  
-	global $paged;
-	if($pages == '')
-	{
-		global $wp_query; 
-		$pages = $wp_query->max_num_pages;
-	
-		if(!$pages)
-			$pages = 1;		 
-	}   
-	
-	if(1 != $pages)
-	{
-	    echo '<nav aria-label="Page navigation" role="navigation">';
-        echo '<span class="sr-only">Page navigation</span>';
-        echo '<ul class="pagination justify-content-center ft-wpbs mb-4">';
-		
-     
-	 	if($paged > 2 && $paged > $range+1 && $showitems < $pages) 
-			echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link(1).'" aria-label="First Page">&laquo;</a></li>';
-	
-	 	if($paged > 1 && $showitems < $pages) 
-			echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link($paged - 1).'" aria-label="Previous Page">&lsaquo;</a></li>';
-	
-		for ($i=1; $i <= $pages; $i++)
-		{
-		    if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
-				echo ($paged == $i)? '<li class="page-item active"><span class="page-link"><span class="sr-only">Current Page </span>'.$i.'</span></li>' : '<li class="page-item"><a class="page-link" href="'.get_pagenum_link($i).'"><span class="sr-only">Page </span>'.$i.'</a></li>';
-		}
-		
-		if ($paged < $pages && $showitems < $pages) 
-			echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link(($paged === 0 ? 1 : $paged) + 1).'" aria-label="Next Page">&rsaquo;</a></li>';  
-	
-	 	if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) 
-			echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link($pages).'" aria-label="Last Page">&raquo;</a></li>';
-	
-	 	echo '</ul>';
-        echo '</nav>';
-        // Uncomment this if you want to show [Page 2 of 30]
-        // echo '<div class="pagination-info mb-5 text-center">[ <span class="text-muted">Page</span> '.$paged.' <span class="text-muted">of</span> '.$pages.' ]</div>';	 	
-	}
-}
+if ( ! function_exists( 'bootscore_pagination' ) ) :
+
+    function bootscore_pagination($pages = '', $range = 2) 
+    {  
+        $showitems = ($range * 2) + 1;  
+        global $paged;
+        if($pages == '')
+        {
+            global $wp_query; 
+            $pages = $wp_query->max_num_pages;
+
+            if(!$pages)
+                $pages = 1;		 
+        }   
+
+        if(1 != $pages)
+        {
+            echo '<nav aria-label="Page navigation" role="navigation">';
+            echo '<span class="sr-only">Page navigation</span>';
+            echo '<ul class="pagination justify-content-center ft-wpbs mb-4">';
+
+
+            if($paged > 2 && $paged > $range+1 && $showitems < $pages) 
+                echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link(1).'" aria-label="First Page">&laquo;</a></li>';
+
+            if($paged > 1 && $showitems < $pages) 
+                echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link($paged - 1).'" aria-label="Previous Page">&lsaquo;</a></li>';
+
+            for ($i=1; $i <= $pages; $i++)
+            {
+                if (1 != $pages &&( !($i >= $paged+$range+1 || $i <= $paged-$range-1) || $pages <= $showitems ))
+                    echo ($paged == $i)? '<li class="page-item active"><span class="page-link"><span class="sr-only">Current Page </span>'.$i.'</span></li>' : '<li class="page-item"><a class="page-link" href="'.get_pagenum_link($i).'"><span class="sr-only">Page </span>'.$i.'</a></li>';
+            }
+
+            if ($paged < $pages && $showitems < $pages) 
+                echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link(($paged === 0 ? 1 : $paged) + 1).'" aria-label="Next Page">&rsaquo;</a></li>';  
+
+            if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) 
+                echo '<li class="page-item"><a class="page-link" href="'.get_pagenum_link($pages).'" aria-label="Last Page">&raquo;</a></li>';
+
+            echo '</ul>';
+            echo '</nav>';
+            // Uncomment this if you want to show [Page 2 of 30]
+            // echo '<div class="pagination-info mb-5 text-center">[ <span class="text-muted">Page</span> '.$paged.' <span class="text-muted">of</span> '.$pages.' ]</div>';	 	
+        }
+    }
+
+endif;
 //Pagination Categories END
 
 
