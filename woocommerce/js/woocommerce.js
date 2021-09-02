a32ba3cb57eb5fb55bb187ff808e9ace13ef500c
Basti <51531217+crftwrk@users.noreply.github.com>
2021-09-02 12:11:45 +0200

    Update woocommerce.js

--- woocommerce/js/woocommerce.js
+++ woocommerce/js/woocommerce.js
@@ -30,7 +30,6 @@ jQuery(document).ready(function ($) {
     $('body').on('blur change', '.form-row input', function () {
         $('.woocommerce form .form-row.woocommerce-validated .select2-container, .woocommerce form .form-row.woocommerce-validated input.input-text, .woocommerce form .form-row.woocommerce-validated select, .woocommerce form .form-row.woocommerce-validated .form-check-input[type=checkbox]').removeClass('is-invalid').addClass('is-valid');
         $('.woocommerce form .form-row.woocommerce-invalid .select2-container, .woocommerce form .form-row.woocommerce-invalid input.input-text, .woocommerce form .form-row.woocommerce-invalid select, .woocommerce form .form-row.woocommerce-invalid .form-check-input[type=checkbox]').removeClass('is-valid').addClass('is-invalid');
-
     });
     // Checkout Form Validation END
 
