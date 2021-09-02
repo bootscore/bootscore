ab3d96837fbe29375d1b04d3237d373d887f75e8
craftwerk <pst@crftwrk.de>
2021-09-02 11:54:33 +0200

    Bootstrap form validation to WooCommerce checkout

--- woocommerce/js/woocommerce.js
+++ woocommerce/js/woocommerce.js
@@ -20,11 +20,18 @@ jQuery(document).ready(function ($) {
     }
     // Keep offcanvas-user open on reload if contains login or register error alert END
 
-
     // Review Checkbox Products
     $('.comment-form-cookies-consent').addClass('form-check');
     $('#wp-comment-cookies-consent').addClass('form-check-input');
     $('.comment-form-cookies-consent label').addClass('form-check-label');
     // Review Checkbox END
+
+    // Checkout Form Validation
+    $('body').on('blur change', '.form-row input', function () {
+        $('.woocommerce form .form-row.woocommerce-validated .select2-container, .woocommerce form .form-row.woocommerce-validated input.input-text, .woocommerce form .form-row.woocommerce-validated select, .woocommerce form .form-row.woocommerce-validated .form-check-input[type=checkbox]').removeClass('is-invalid').addClass('is-valid');
+        $('.woocommerce form .form-row.woocommerce-invalid .select2-container, .woocommerce form .form-row.woocommerce-invalid input.input-text, .woocommerce form .form-row.woocommerce-invalid select, .woocommerce form .form-row.woocommerce-invalid .form-check-input[type=checkbox]').removeClass('is-valid').addClass('is-invalid');
+
+    });
+    // Checkout Form Validation END
 
 }); // jQuery End
