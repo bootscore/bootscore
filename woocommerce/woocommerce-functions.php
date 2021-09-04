9ff2d7744d18beb3ad1ca0737f7140d40f6b30b9
craftwerk <pst@crftwrk.de>
2021-09-04 19:24:10 +0200

    Filter class card-img-top to product loop img

--- woocommerce/woocommerce-functions.php
+++ woocommerce/woocommerce-functions.php
@@ -277,3 +277,18 @@ function custom_empty_cart_message() {
     echo $html . '</div>';
 }
 // Cart empty message alert End
+
+
+// Add card-img-top class to product loop
+remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+add_action( 'woocommerce_before_shop_loop_item_title', 'custom_loop_product_thumbnail', 10 );
+function custom_loop_product_thumbnail() {
+    global $product;
+    $size = 'woocommerce_thumbnail';
+    $code = 'class=card-img-top';
+
+    $image_size = apply_filters( 'single_product_archive_thumbnail_size', $size );
+
+    echo $product ? $product->get_image( $image_size, $code ) : '';
+}
+// Add card-img-top class to product loop End
