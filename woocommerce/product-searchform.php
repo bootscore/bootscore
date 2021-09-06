728d5278d323214499757a386cd849a05e5ed052
craftwerk <pst@crftwrk.de>
2021-09-06 10:12:48 +0200

    Update product-searchform.php

--- woocommerce/product-searchform.php
+++ woocommerce/product-searchform.php
@@ -26,8 +26,6 @@ if ( ! defined( 'ABSPATH' ) ) {
         <input class="form-control" type="search" id="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>" class="search-field field form-control" placeholder="<?php echo esc_attr__( 'Search products...', 'woocommerce' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
         <label class="sr-only" for="woocommerce-product-search-field-<?php echo isset( $index ) ? absint( $index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'woocommerce' ); ?></label>
         <input type="hidden" name="post_type" value="product" />
-    
-            <button class="input-group-text btn btn-outline-secondary" type="submit"><i class="fas fa-search"></i></button>
-     
+        <button class="input-group-text btn btn-outline-secondary" type="submit"><i class="fas fa-search"></i></button>     
     </div>
 </form>
