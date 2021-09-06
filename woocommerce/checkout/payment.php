b2738d893782f0c1455f77e33da3cb31246a8d48
craftwerk <pst@crftwrk.de>
2021-09-06 10:51:48 +0200

    Improved checkout

--- woocommerce/checkout/payment.php
+++ woocommerce/checkout/payment.php
@@ -21,7 +21,7 @@ if ( ! is_ajax() ) {
 	do_action( 'woocommerce_review_order_before_payment' );
 }
 ?>
-<div id="payment" class="woocommerce-checkout-payment card bg-transparent">
+<div id="payment" class="woocommerce-checkout-payment card bg-transparent rounded">
     <div class="card-body">
         <?php if ( WC()->cart->needs_payment() ) : ?>
         <div class="wc_payment_methods payment_methods methods p-0 border-bottom">
