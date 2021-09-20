5bc2aad6e01929e09626cf2da6641115f3d396d8
Justin Kruit <justin@indivsolutions.nl>
2021-09-20 12:46:27 +0200

    Add the compile function

--- inc/scss-compiler.php
+++ inc/scss-compiler.php
@@ -1,7 +1,17 @@
 <?php
 
 /**
- * Functions to compile scss in the theme itself - TODO
+ * Functions to compile scss in the theme itself
  *
  * @package Bootscore
  */
+
+require_once "scssphp/scss.inc.php";
+
+use ScssPhp\ScssPhp\Compiler;
+
+function bootscore_compile_scss() {
+  $compiler = new Compiler();
+
+  // TODO: write the compiler
+}
