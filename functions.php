92e0c16812da14d01c202fd73fc53176de413154
Justin Kruit <justinkruitnl@gmail.com>
2021-09-24 13:46:09 +0200

    Check if files have been edited before compiling

--- functions.php
+++ functions.php
@@ -246,7 +246,7 @@ function bootscore_scripts() {
 
   // Get modification time. Enqueue files with modification date to prevent browser from loading cached scripts and styles when file content changes.
   $modificated_styleCss = date('YmdHi', filemtime(get_stylesheet_directory() . '/style.css'));
-  $modificated_bootstrapCss = date('YmdHi', filemtime(get_template_directory() . '/css/lib/bootstrap.min.css'));
+  $modificated_bootscoreCss = date('YmdHi', filemtime(get_template_directory() . '/css/main.css'));
   $modificated_fontawesomeCss = date('YmdHi', filemtime(get_template_directory() . '/css/lib/fontawesome.min.css'));
   $modificated_bootstrapJs = date('YmdHi', filemtime(get_template_directory() . '/js/lib/bootstrap.bundle.min.js'));
   $modificated_themeJs = date('YmdHi', filemtime(get_template_directory() . '/js/theme.js'));
@@ -257,6 +257,7 @@ function bootscore_scripts() {
   // Bootstrap	
   // wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/lib/bootstrap.min.css', array(), $modificated_bootstrapCss);
   require_once 'inc/scss-compiler.php';
+  wp_enqueue_style('bootscore', get_template_directory_uri() . '/css/main.css', array(), $modificated_bootscoreCss);
     
   // Fontawesome
   wp_enqueue_style('fontawesome', get_template_directory_uri() . '/css/lib/fontawesome.min.css', array(), $modificated_fontawesomeCss);
--- inc/scss-compiler.php
+++ inc/scss-compiler.php
@@ -17,9 +17,36 @@ function bootscore_compile_scss() {
   $css_file = get_stylesheet_directory() . '/css/main.css';
 
   $compiler->setImportPaths(dirname($scss_file));
-  $compiled = $compiler->compileString(file_get_contents($scss_file));
-  file_put_contents($css_file, $compiled->getCss());
-  // TODO: write the compiler
+  if (is_child_theme()) {
+    $compiler->addImportPath(get_template_directory() . '/css/lib/');
+  }
+
+  $last_modified = bootscore_get_last_modified_scss();
+  $stored_modified = get_theme_mod('bootscore_scss_modified_timestamp', 0);
+
+  try {
+    if ($last_modified > $stored_modified) {
+      $compiled = $compiler->compileString(file_get_contents($scss_file));
+      file_put_contents($css_file, $compiled->getCss());
+
+      set_theme_mod('bootscore_scss_modified_timestamp', $last_modified);
+    }
+  } catch (Exception $e) {
+    wp_die('<b>bootScore SCSS Compiler - Caught exception:</b><br><br> ' . $e->getMessage());
+  }
 }
 
 bootscore_compile_scss();
+
+function bootscore_get_last_modified_scss() {
+  $directory = get_stylesheet_directory() . '/css/lib/';
+  $files = scandir($directory);
+  $total_last_modified = 0;
+  foreach ($files as $file) {
+    if (strpos($file, '.scss') !== false || strpos($file, '.css') !== false) {
+      $file_stats = stat($directory . $file);
+      $total_last_modified = $file_stats['mtime'];
+    }
+  }
+  return $total_last_modified;
+}
