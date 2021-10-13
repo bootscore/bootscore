ee60385e56c1b30cda1f2fa02f6857913a4dcc4b
Justin Kruit <justinkruitnl@gmail.com>
2021-10-13 19:14:47 +0200

    Fix woocommerce-functions.php formatting

--- woocommerce/woocommerce-functions.php
+++ woocommerce/woocommerce-functions.php
@@ -57,130 +57,128 @@ add_action('wp_enqueue_scripts', 'wc_scripts');
 
 // Minicart Header
 if (!function_exists('bs_mini_cart')) :
-
   function bs_mini_cart($fragments) {
 
     ob_start();
-    $count = WC()->cart->cart_contents_count;
-?><span class="cart-content"><?php
-                                      if ($count > 0) {
-                                      ?>
+    $count = WC()->cart->cart_contents_count; ?>
+    <span class="cart-content">
+      <?php if ($count > 0) { ?>
         <span class="cart-content-count position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger border border-light"><?php echo esc_html($count); ?></span><span class="cart-total ms-1 d-none d-md-inline"><?php echo WC()->cart->get_cart_subtotal(); ?></span>
-      <?php
-                                      }
-      ?></span><?php
+      <?php } ?>
+    </span>
 
-                      $fragments['span.cart-content'] = ob_get_clean();
+<?php
+    $fragments['span.cart-content'] = ob_get_clean();
 
-                      return $fragments;
-                    }
-                    add_filter('woocommerce_add_to_cart_fragments', 'bs_mini_cart');
+    return $fragments;
+  }
+  add_filter('woocommerce_add_to_cart_fragments', 'bs_mini_cart');
 
-                  endif;
-                  // Minicrt Header End
-
-
-                  // WooCommerce Breadcrumb
-                  if (!function_exists('bs_woocommerce_breadcrumbs')) :
-                    add_filter('woocommerce_breadcrumb_defaults', 'bs_woocommerce_breadcrumbs');
-                    function bs_woocommerce_breadcrumbs() {
-                      return array(
-                        'delimiter'   => ' &nbsp;&#47;&nbsp; ',
-                        'wrap_before' => '<nav class="breadcrumb mb-4 mt-2 bg-light py-2 px-3 small rounded" itemprop="breadcrumb">',
-                        'wrap_after'  => '</nav>',
-                        'before'      => '',
-                        'after'       => '',
-                        'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
-                      );
-                    }
-                  endif;
-                  // WooCommerce Breadcrumb End
+endif;
+// Minicart Header End
 
 
-                  // Optional Telephone
-                  if (!function_exists('evolution_phone_no_pflicht')) :
-
-                    function evolution_phone_no_pflicht($address_fields) {
-                      $address_fields['billing_phone']['required'] = false;
-                      return $address_fields;
-                    }
-                    add_filter('woocommerce_billing_fields', 'evolution_phone_no_pflicht', 10, 1);
-                  endif;
-                  // Optional Telephone End
-
-
-                  // Bootstrap Billing forms
-                  function iap_wc_bootstrap_form_field_args($args, $key, $value) {
-
-                    $args['input_class'][] = 'form-control';
-                    return $args;
-                  }
-                  add_filter('woocommerce_form_field_args', 'iap_wc_bootstrap_form_field_args', 10, 3);
-                  // Bootstrap Billing forms End
+// WooCommerce Breadcrumb
+if (!function_exists('bs_woocommerce_breadcrumbs')) :
+  add_filter('woocommerce_breadcrumb_defaults', 'bs_woocommerce_breadcrumbs');
+  function bs_woocommerce_breadcrumbs() {
+    return array(
+      'delimiter'   => ' &nbsp;&#47;&nbsp; ',
+      'wrap_before' => '<nav class="breadcrumb mb-4 mt-2 bg-light py-2 px-3 small rounded" itemprop="breadcrumb">',
+      'wrap_after'  => '</nav>',
+      'before'      => '',
+      'after'       => '',
+      'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
+    );
+  }
+endif;
+// WooCommerce Breadcrumb End
 
 
-                  // Ship to a different address closed by default
-                  add_filter('woocommerce_ship_to_different_address_checked', '__return_false');
-                  // Ship to a different address closed by default End
+// Optional Telephone
+if (!function_exists('evolution_phone_no_pflicht')) :
+
+  function evolution_phone_no_pflicht($address_fields) {
+    $address_fields['billing_phone']['required'] = false;
+    return $address_fields;
+  }
+  add_filter('woocommerce_billing_fields', 'evolution_phone_no_pflicht', 10, 1);
+endif;
+// Optional Telephone End
 
 
-                  // Remove cross-sells at cart
-                  remove_action('woocommerce_cart_collaterals', 'woocommerce_cross_sell_display');
-                  // Remove cross-sells at cart End
+// Bootstrap Billing forms
+function iap_wc_bootstrap_form_field_args($args, $key, $value) {
+
+  $args['input_class'][] = 'form-control';
+  return $args;
+}
+add_filter('woocommerce_form_field_args', 'iap_wc_bootstrap_form_field_args', 10, 3);
+// Bootstrap Billing forms End
 
 
-                  // Remove CSS and/or JS for Select2 used by WooCommerce, see https://gist.github.com/Willem-Siebe/c6d798ccba249d5bf080.
-                  add_action('wp_enqueue_scripts', 'wsis_dequeue_stylesandscripts_select2', 100);
-
-                  function wsis_dequeue_stylesandscripts_select2() {
-                    if (class_exists('woocommerce')) {
-                      wp_dequeue_style('selectWoo');
-                      wp_deregister_style('selectWoo');
-
-                      wp_dequeue_script('selectWoo');
-                      wp_deregister_script('selectWoo');
-                    }
-                  }
-                  // Remove CSS and/or JS for Select2 END
+// Ship to a different address closed by default
+add_filter('woocommerce_ship_to_different_address_checked', '__return_false');
+// Ship to a different address closed by default End
 
 
-                  // Mini cart widget buttons
-                  remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_button_view_cart', 10);
-                  remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout', 20);
-
-                  function my_woocommerce_widget_shopping_cart_button_view_cart() {
-                    echo '<a href="' . esc_url(wc_get_cart_url()) . '" class="btn btn-outline-primary d-block mb-2">' . esc_html__('View cart', 'woocommerce') . '</a>';
-                  }
-                  function my_woocommerce_widget_shopping_cart_proceed_to_checkout() {
-                    echo '<a href="' . esc_url(wc_get_checkout_url()) . '" class="btn btn-primary d-block">' . esc_html__('Checkout', 'woocommerce') . '</a>';
-                  }
-                  add_action('woocommerce_widget_shopping_cart_buttons', 'my_woocommerce_widget_shopping_cart_button_view_cart', 10);
-                  add_action('woocommerce_widget_shopping_cart_buttons', 'my_woocommerce_widget_shopping_cart_proceed_to_checkout', 20);
-                  // Mini cart widget buttons End
+// Remove cross-sells at cart
+remove_action('woocommerce_cart_collaterals', 'woocommerce_cross_sell_display');
+// Remove cross-sells at cart End
 
 
-                  // Cart empty message alert
-                  remove_action('woocommerce_cart_is_empty', 'wc_empty_cart_message', 10);
-                  add_action('woocommerce_cart_is_empty', 'custom_empty_cart_message', 10);
+// Remove CSS and/or JS for Select2 used by WooCommerce, see https://gist.github.com/Willem-Siebe/c6d798ccba249d5bf080.
+add_action('wp_enqueue_scripts', 'wsis_dequeue_stylesandscripts_select2', 100);
 
-                  function custom_empty_cart_message() {
-                    $html  = '<div class="cart-empty alert alert-info">';
-                    $html .= wp_kses_post(apply_filters('wc_empty_cart_message', __('Your cart is currently empty.', 'woocommerce')));
-                    echo $html . '</div>';
-                  }
-                  // Cart empty message alert End
+function wsis_dequeue_stylesandscripts_select2() {
+  if (class_exists('woocommerce')) {
+    wp_dequeue_style('selectWoo');
+    wp_deregister_style('selectWoo');
+
+    wp_dequeue_script('selectWoo');
+    wp_deregister_script('selectWoo');
+  }
+}
+// Remove CSS and/or JS for Select2 END
 
 
-                  // Add card-img-top class to product loop
-                  remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
-                  add_action('woocommerce_before_shop_loop_item_title', 'custom_loop_product_thumbnail', 10);
-                  function custom_loop_product_thumbnail() {
-                    global $product;
-                    $size = 'woocommerce_thumbnail';
-                    $code = 'class=card-img-top';
+// Mini cart widget buttons
+remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_button_view_cart', 10);
+remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout', 20);
 
-                    $image_size = apply_filters('single_product_archive_thumbnail_size', $size);
+function my_woocommerce_widget_shopping_cart_button_view_cart() {
+  echo '<a href="' . esc_url(wc_get_cart_url()) . '" class="btn btn-outline-primary d-block mb-2">' . esc_html__('View cart', 'woocommerce') . '</a>';
+}
+function my_woocommerce_widget_shopping_cart_proceed_to_checkout() {
+  echo '<a href="' . esc_url(wc_get_checkout_url()) . '" class="btn btn-primary d-block">' . esc_html__('Checkout', 'woocommerce') . '</a>';
+}
+add_action('woocommerce_widget_shopping_cart_buttons', 'my_woocommerce_widget_shopping_cart_button_view_cart', 10);
+add_action('woocommerce_widget_shopping_cart_buttons', 'my_woocommerce_widget_shopping_cart_proceed_to_checkout', 20);
+// Mini cart widget buttons End
 
-                    echo $product ? $product->get_image($image_size, $code) : '';
-                  }
+
+// Cart empty message alert
+remove_action('woocommerce_cart_is_empty', 'wc_empty_cart_message', 10);
+add_action('woocommerce_cart_is_empty', 'custom_empty_cart_message', 10);
+
+function custom_empty_cart_message() {
+  $html  = '<div class="cart-empty alert alert-info">';
+  $html .= wp_kses_post(apply_filters('wc_empty_cart_message', __('Your cart is currently empty.', 'woocommerce')));
+  echo $html . '</div>';
+}
+// Cart empty message alert End
+
+
+// Add card-img-top class to product loop
+remove_action('woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10);
+add_action('woocommerce_before_shop_loop_item_title', 'custom_loop_product_thumbnail', 10);
+function custom_loop_product_thumbnail() {
+  global $product;
+  $size = 'woocommerce_thumbnail';
+  $code = 'class=card-img-top';
+
+  $image_size = apply_filters('single_product_archive_thumbnail_size', $size);
+
+  echo $product ? $product->get_image($image_size, $code) : '';
+}
 // Add card-img-top class to product loop End
