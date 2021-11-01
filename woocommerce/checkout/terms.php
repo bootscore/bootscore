7b4c3cc9ff01280de392d4790bd636c15f3181fb
Justin Kruit <justinkruitnl@gmail.com>
2021-11-01 18:59:23 +0100

    Reformat Pull Request #32

--- woocommerce/checkout/terms.php
+++ woocommerce/checkout/terms.php
@@ -9,11 +9,10 @@
 
 defined('ABSPATH') || exit;
 
-if (apply_filters('woocommerce_checkout_show_terms', true) && function_exists('wc_terms_and_conditions_checkbox_enabled'))
-{
+if (apply_filters('woocommerce_checkout_show_terms', true) && function_exists('wc_terms_and_conditions_checkbox_enabled')) {
 
   echo bootscore_wc_get_corrected_terms(bootscore_wc_capture_hook_output('woocommerce_checkout_before_terms_and_conditions'));
-  ?>
+?>
   <div class="woocommerce-terms-and-conditions-wrapper">
     <?php
     /**
@@ -29,13 +28,14 @@ if (apply_filters('woocommerce_checkout_show_terms', true) && function_exists('w
     <?php if (wc_terms_and_conditions_checkbox_enabled()) : ?>
       <p class="form-row validate-required">
         <span class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox form-check">
-          <input type="checkbox" id="terms" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox form-check-input" name="terms" <?php checked(apply_filters('woocommerce_terms_is_checked_default', isset($_POST['terms'])), true); // WPCS: input var ok, csrf ok. ?> />
+          <input type="checkbox" id="terms" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox form-check-input" name="terms" <?php checked(apply_filters('woocommerce_terms_is_checked_default', isset($_POST['terms'])), true); // WPCS: input var ok, csrf ok. 
+                                                                                                                                                          ?> />
           <label class="woocommerce-terms-and-conditions-checkbox-text form-check-label" for="terms"><?php wc_terms_and_conditions_checkbox_text(); ?>&nbsp;<span class="required">*</span></label>
         </span>
         <input type="hidden" name="terms-field" value="1" />
       </p>
     <?php endif; ?>
   </div>
-  <?php
-    echo bootscore_wc_get_corrected_terms(bootscore_wc_capture_hook_output('woocommerce_checkout_after_terms_and_conditions'));
+<?php
+  echo bootscore_wc_get_corrected_terms(bootscore_wc_capture_hook_output('woocommerce_checkout_after_terms_and_conditions'));
 }
--- woocommerce/woocommerce-functions.php
+++ woocommerce/woocommerce-functions.php
@@ -183,65 +183,56 @@ function custom_loop_product_thumbnail() {
 }
 // Add card-img-top class to product loop End
 
-  
-  /**
-   * Get the corrected terms for Woocommerce.
-   *
-   * @param  string $html The original terms.
-   * @return string The corrected terms.
-   */
-  function bootscore_wc_get_corrected_terms($html) {
-    $doc = new DOMDocument();
-    if ( !empty($html) && $doc->loadHtml($html))
-    {
-      $documentElement = $doc->documentElement; // Won't find the right child-notes without that line. ads html and body tag as a wrapper
-      $somethingWasCorrected = false;
-      foreach ($documentElement->childNodes[0]->childNodes as $mainNode) {
-        if($mainNode->childNodes->length && strpos($mainNode->getAttribute("class"), "form-row") !== false)
-        {
-          if(strpos($mainNode->getAttribute("class"), "required") !== false)
-          {
-            $mainNode->setAttribute("class", "form-row validate-required"); // You could try to keep the original class and only add the string, but I think that could ruin the design
-          }
-          else
-          {
-            $mainNode->setAttribute("class", "form-row woocommerce-validated");
-          }
-          $nodesLabel = $mainNode->getElementsByTagName("label");
-          if ($nodesLabel->length) {
-            $nodesLabel[0]->setAttribute("class", "woocommerce-form__label woocommerce-form__label-for-checkbox checkbox form-check display-inline-block d-inline-block");
-          }
-          $nodesInput = $mainNode->getElementsByTagName( "input" );
-          if ($nodesInput->length ) {
-            $nodesInput[0]->setAttribute("class", "woocommerce-form__input woocommerce-form__input-checkbox input-checkbox form-check-input");
-          }
-          $somethingWasCorrected = true;
+
+/**
+ * Get the corrected terms for Woocommerce.
+ *
+ * @param  string $html The original terms.
+ * @return string The corrected terms.
+ */
+function bootscore_wc_get_corrected_terms($html) {
+  $doc = new DOMDocument();
+  if (!empty($html) && $doc->loadHtml($html)) {
+    $documentElement = $doc->documentElement; // Won't find the right child-notes without that line. ads html and body tag as a wrapper
+    $somethingWasCorrected = false;
+    foreach ($documentElement->childNodes[0]->childNodes as $mainNode) {
+      if ($mainNode->childNodes->length && strpos($mainNode->getAttribute("class"), "form-row") !== false) {
+        if (strpos($mainNode->getAttribute("class"), "required") !== false) {
+          $mainNode->setAttribute("class", "form-row validate-required"); // You could try to keep the original class and only add the string, but I think that could ruin the design
+        } else {
+          $mainNode->setAttribute("class", "form-row woocommerce-validated");
         }
-      }
-      if($somethingWasCorrected) {
-        return $doc->saveHTML();
-      }
-      else
-      {
-        return $html;
+        $nodesLabel = $mainNode->getElementsByTagName("label");
+        if ($nodesLabel->length) {
+          $nodesLabel[0]->setAttribute("class", "woocommerce-form__label woocommerce-form__label-for-checkbox checkbox form-check display-inline-block d-inline-block");
+        }
+        $nodesInput = $mainNode->getElementsByTagName("input");
+        if ($nodesInput->length) {
+          $nodesInput[0]->setAttribute("class", "woocommerce-form__input woocommerce-form__input-checkbox input-checkbox form-check-input");
+        }
+        $somethingWasCorrected = true;
       }
     }
-    else
-    {
-      //error maybe return $html?
+    if ($somethingWasCorrected) {
+      return $doc->saveHTML();
+    } else {
+      return $html;
     }
+  } else {
+    //error maybe return $html?
   }
-    
-  /**
-   * Capture the output of a hook.
-   *
-   * @param  string $hookName The name of the hook to capture.
-   * @return string The output of the hook.
-   */
-  function bootscore_wc_capture_hook_output($hookName) {
-    ob_start();
-    do_action($hookName);
-    $hookContent = ob_get_contents();
-    ob_end_clean();
-    return $hookContent;
-  }
+}
+
+/**
+ * Capture the output of a hook.
+ *
+ * @param  string $hookName The name of the hook to capture.
+ * @return string The output of the hook.
+ */
+function bootscore_wc_capture_hook_output($hookName) {
+  ob_start();
+  do_action($hookName);
+  $hookContent = ob_get_contents();
+  ob_end_clean();
+  return $hookContent;
+}
