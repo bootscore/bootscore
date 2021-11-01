b0eb6394973f969241ae8d94ba93cb06d1fcff7c
MH <martin@risingbytes.at>
2021-11-01 15:53:45 +0100

    moved filter functions to woocommerce-functions.php

--- woocommerce/woocommerce-functions.php
+++ woocommerce/woocommerce-functions.php
@@ -182,3 +182,57 @@ function custom_loop_product_thumbnail() {
   echo $product ? $product->get_image($image_size, $code) : '';
 }
 // Add card-img-top class to product loop End
+
+// Add filter function for bootstrap syntax in terms.php
+
+  function getCorrectedTerms($html) {
+    $doc = new DOMDocument();
+    if ( !empty($html) && $doc->loadHtml($html))
+    {
+      $documentElement = $doc->documentElement; // Won't find the right child-notes without that line. ads html and body tag as a wrapper
+      $somethingWasCorrected = false;
+      foreach ($documentElement->childNodes[0]->childNodes as $mainNode) {
+        if($mainNode->childNodes->length && strpos($mainNode->getAttribute("class"), "form-row") !== false)
+        {
+          if(strpos($mainNode->getAttribute("class"), "required") !== false)
+          {
+            $mainNode->setAttribute("class", "form-row validate-required"); // You could try to keep the original class and only add the string, but I think that could ruin the design
+          }
+          else
+          {
+            $mainNode->setAttribute("class", "form-row woocommerce-validated");
+          }
+          $nodesLabel = $mainNode->getElementsByTagName("label");
+          if ($nodesLabel->length) {
+            $nodesLabel[0]->setAttribute("class", "woocommerce-form__label woocommerce-form__label-for-checkbox checkbox form-check display-inline-block d-inline-block");
+          }
+          $nodesInput = $mainNode->getElementsByTagName( "input" );
+          if ($nodesInput->length ) {
+            $nodesInput[0]->setAttribute("class", "woocommerce-form__input woocommerce-form__input-checkbox input-checkbox form-check-input");
+          }
+          $somethingWasCorrected = true;
+        }
+      }
+      if($somethingWasCorrected) {
+        return $doc->saveHTML();
+      }
+      else
+      {
+        return $html;
+      }
+    }
+    else
+    {
+      //error maybe return $html?
+    }
+  }
+  
+  function captureHookOutput($hookName) {
+    ob_start(); // start capturing output.
+    do_action($hookName);
+    $hookContent = ob_get_contents(); // the actions output will now be stored in the variable as a string!
+    ob_end_clean(); // never forget this or you will keep capturing output.
+    return $hookContent;
+  }
+
+// Add filter function for bootstrap syntax in terms.php
