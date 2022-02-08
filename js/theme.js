90868da110416a50ffed6c0e6b733da420a7c25e
Laurent Binder <laurentbinder@gmail.com>
2022-02-08 17:14:45 +0100

    moved and renamed  qty_btn files into WC

--- js/theme.js
+++ js/theme.js
@@ -101,49 +101,4 @@ jQuery(function ($) {
   $('.height-85').css('height', 0.85 * $(window).height());
   $('.height-100').css('height', 1.0 * $(window).height());
 
-  // WC Quantity Input
-  if (!String.prototype.getDecimals) {
-    String.prototype.getDecimals = function () {
-      var num = this,
-        match = ('' + num).match(/(?:\.(\d+))?(?:[eE]([+-]?\d+))?$/);
-      if (!match) {
-        return 0;
-      }
-      return Math.max(0, (match[1] ? match[1].length : 0) - (match[2] ? +match[2] : 0));
-    }
-  }
-  // Quantity "plus" and "minus" buttons
-  $(document.body).on('click', '.plus, .minus', function () {
-    var $qty = $(this).closest('.quantity').find('.qty'),
-      currentVal = parseFloat($qty.val()),
-      max = parseFloat($qty.attr('max')),
-      min = parseFloat($qty.attr('min')),
-      step = $qty.attr('step');
-
-    // Format values
-    if (!currentVal || currentVal === '' || currentVal === 'NaN') currentVal = 0;
-    if (max === '' || max === 'NaN') max = '';
-    if (min === '' || min === 'NaN') min = 0;
-    if (step === 'any' || step === '' || step === undefined || parseFloat(step) === 'NaN') step = 1;
-
-    // Change the value
-    if ($(this).is('.plus')) {
-      if (max && (currentVal >= max)) {
-        $qty.val(max);
-      } else {
-        $qty.val((currentVal + parseFloat(step)).toFixed(step.getDecimals()));
-      }
-    } else {
-      if (min && (currentVal <= min)) {
-        $qty.val(min);
-      } else if (currentVal > 0) {
-        $qty.val((currentVal - parseFloat(step)).toFixed(step.getDecimals()));
-      }
-    }
-
-    // Trigger change event
-    $qty.trigger('change');
-  });
-  // WC Quantity Input End
-
 }); // jQuery End
