090c63691e0366a367a5a47239bb5d17d2c1049e
Justin Kruit <justin@indivsolutions.nl>
2022-04-21 09:13:37 +0200

    $labelledby does not exist

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -32,7 +32,7 @@ if ($max_value && $min_value === $max_value) {
     <?php do_action('woocommerce_before_quantity_input_field'); ?>
     <button type="button" class="input-group-text qty_button minus">-</button>
     <label class="screen-reader-text" for="<?php echo esc_attr($input_id); ?>"><?php esc_html_e('Quantity', 'woocommerce'); ?></label>
-    <input type="number" id="<?php echo esc_attr($input_id); ?>" class="input-text qty text form-control" step="<?php echo esc_attr($step); ?>" min="<?php echo esc_attr($min_value); ?>" max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>" size="4" pattern="<?php echo esc_attr($pattern); ?>" inputmode="<?php echo esc_attr($inputmode); ?>" aria-labelledby="<?php echo esc_attr($labelledby); ?>" />
+    <input type="number" id="<?php echo esc_attr($input_id); ?>" class="input-text qty text form-control" step="<?php echo esc_attr($step); ?>" min="<?php echo esc_attr($min_value); ?>" max="<?php echo esc_attr(0 < $max_value ? $max_value : ''); ?>" name="<?php echo esc_attr($input_name); ?>" value="<?php echo esc_attr($input_value); ?>" title="<?php echo esc_attr_x('Qty', 'Product quantity input tooltip', 'woocommerce'); ?>" size="4" pattern="<?php echo esc_attr($pattern); ?>" inputmode="<?php echo esc_attr($inputmode); ?>" />
     <button type="button" class="input-group-text qty_button plus">+</button>
     <?php do_action('woocommerce_after_quantity_input_field'); ?>
   </div>
