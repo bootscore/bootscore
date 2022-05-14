ea066fbf120641fff36e922928dbcc8f6cf95be1
craftwerk <pst@crftwrk.de>
2022-05-14 12:42:39 +0200

    Remove `navbar-light` classes

--- header-woocommerce.php
+++ header-woocommerce.php
@@ -41,7 +41,7 @@
 
       <div class="fixed-top bg-light">
 
-        <nav id="nav-main" class="navbar navbar-expand-lg navbar-light">
+        <nav id="nav-main" class="navbar navbar-expand-lg">
 
           <div class="container">
 
