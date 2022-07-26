e7e633169665f03a4472d062c21af19ae1ca4e1b
craftwerk <pst@crftwrk.de>
2022-07-26 10:56:27 +0200

    Minor formatting functions.php

--- functions.php
+++ functions.php
@@ -48,35 +48,35 @@ if (!function_exists('bootscore_setup')) :
    */
   function bootscore_setup() {
     /*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on Bootscore, use a find and replace
-		 * to change 'bootscore' to the name of your theme in all the template files.
-		 */
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on Bootscore, use a find and replace
+     * to change 'bootscore' to the name of your theme in all the template files.
+    */
     load_theme_textdomain('bootscore', get_template_directory() . '/languages');
 
     // Add default posts and comments RSS feed links to head.
     add_theme_support('automatic-feed-links');
 
     /*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
+    */
     add_theme_support('title-tag');
 
     /*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+    */
     add_theme_support('post-thumbnails');
 
     /*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+    */
     add_theme_support('html5', array(
       'comment-form',
       'comment-list',
