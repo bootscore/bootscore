dc4ddbc76e558eed0b64fb9cfef6d0a8cfd9aaa7
craftwerk <pst@crftwrk.de>
2022-10-08 19:06:21 +0200

    Improved breadcrumb classes

--- woocommerce/woocommerce-functions.php
+++ woocommerce/woocommerce-functions.php
@@ -84,8 +84,8 @@ if (!function_exists('bs_woocommerce_breadcrumbs')) :
   function bs_woocommerce_breadcrumbs() {
     return array(
       'delimiter'   => '',
-      'wrap_before' => "<nav aria-label='breadcrumb' class='wc-breadcrumb breadcrumb-scroller p-3'>
-      <ol class='breadcrumb'>",
+      'wrap_before' => "<nav aria-label='breadcrumb' class='wc-breadcrumb breadcrumb-scroller mb-4 mt-2 py-2 px-3 bg-light rounded'>
+      <ol class='breadcrumb mb-0'>",
       'wrap_after'  => '</ol>
       </nav>',
       'before'      => '<li class="breadcrumb-item">',
