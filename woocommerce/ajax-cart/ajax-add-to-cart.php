e2d6289a05088b33242f1834b5eff645e6720c70
craftwerk <pst@crftwrk.de>
2022-12-10 17:25:52 +0000

    Improve btn-loader

--- woocommerce/ajax-cart/ajax-add-to-cart.php
+++ woocommerce/ajax-cart/ajax-add-to-cart.php
@@ -172,17 +172,24 @@ function bootscore_product_page_ajax_add_to_cart_js() {
       });
 
       
-      
+      /*
   // Add loading spinner to add_to_cart_button 
   $('.single_add_to_cart_button:not(.product_type_variable.single_add_to_cart_button):not(.product_type_grouped.single_add_to_cart_button):not(.product_type_external.single_add_to_cart_button):not(.product-type-external .single_add_to_cart_button):not(.product-type-external .single_add_to_cart_button)').click(function () {
     $(this).prepend('<span class="spinner-border spinner-border-sm me-1"></span>');
   });
+  */
+      
+        // Add loading spinner to add_to_cart_button 
+  $('.single_add_to_cart_button:not(.product_type_variable.single_add_to_cart_button):not(.product_type_grouped.single_add_to_cart_button):not(.product_type_external.single_add_to_cart_button):not(.product-type-external .single_add_to_cart_button):not(.product-type-external .single_add_to_cart_button)').click(function () {
+    $(this).prepend('<div class="btn-loader"><span class="spinner-border spinner-border-sm"></span></div>');
+  });
+
 
   $('body').on('added_to_cart', function () {
     // Open offcanvas-cart when cart is loaded
     $('#offcanvas-cart').offcanvas('show');
     // Remove loading spinner
-    $('.single_add_to_cart_button .spinner-border').remove();
+    $('.single_add_to_cart_button .btn-loader').remove();
   });
 
   // Hide alert in offcanvas-cart when offcanvas is closed
