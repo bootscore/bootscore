598d1f1b4454f8826985a7c2210568bd5a814fe1
Basti <51531217+crftwrk@users.noreply.github.com>
2022-12-17 11:33:09 +0100

    Add title to `single_add_to_cart_button` in loop

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -26,8 +26,9 @@ global $product;
 echo apply_filters(
   'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
   sprintf(
-    '<div class="add-to-cart-container mt-auto"><a href="%s" data-quantity="%s" class="%s product_type_%s single_add_to_cart_button btn btn-primary d-block %s" %s> %s</a></div>',
+    '<div class="add-to-cart-container mt-auto"><a href="%s" title="%s" data-quantity="%s" class="%s product_type_%s single_add_to_cart_button btn btn-primary d-block %s" %s> %s</a></div>',
     esc_url($product->add_to_cart_url()),
+    esc_attr( $product->get_title()),
     esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
     $product->is_purchasable() && $product->is_in_stock() ? 'add_to_cart_button' : '',
     esc_attr($product->get_type()),
