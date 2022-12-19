1e1ef2dac27efe25625a39ad44a1e36b703ab37b
craftwerk <pst@crftwrk.de>
2022-12-19 10:05:42 +0100

    Move back the ajax header minicart to woocommerce-functions.php

--- woocommerce/ajax-cart/ajax-add-to-cart.php
+++ woocommerce/ajax-cart/ajax-add-to-cart.php
@@ -281,26 +281,3 @@ add_filter('woocommerce_add_to_cart_fragments', 'bootscore_ajax_add_to_cart_add_
 // Stop redirecting after stock error
 add_filter( 'woocommerce_cart_redirect_after_error', '__return_false' );
 
-
-
-// Minicart Header
-if (!function_exists('bs_mini_cart')) :
-  function bs_mini_cart($fragments) {
-
-    ob_start();
-    $count = WC()->cart->cart_contents_count; ?>
-    <span class="cart-content">
-      <?php if ($count > 0) { ?>
-        <span class="cart-content-count position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger border border-light"><?php echo esc_html($count); ?></span><span class="cart-total ms-1 d-none d-md-inline"><?php echo WC()->cart->get_cart_subtotal(); ?></span>
-      <?php } ?>
-    </span>
-
-  <?php
-    $fragments['span.cart-content'] = ob_get_clean();
-
-    return $fragments;
-  }
-  add_filter('woocommerce_add_to_cart_fragments', 'bs_mini_cart');
-
-endif;
-// Minicart Header End
--- woocommerce/woocommerce-functions.php
+++ woocommerce/woocommerce-functions.php
@@ -55,6 +55,29 @@ add_action('wp_enqueue_scripts', 'wc_scripts');
 //Scripts and styles End
 
 
+// Minicart Header
+if (!function_exists('bs_mini_cart')) :
+  function bs_mini_cart($fragments) {
+
+    ob_start();
+    $count = WC()->cart->cart_contents_count; ?>
+    <span class="cart-content">
+      <?php if ($count > 0) { ?>
+        <span class="cart-content-count position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger border border-light"><?php echo esc_html($count); ?></span><span class="cart-total ms-1 d-none d-md-inline"><?php echo WC()->cart->get_cart_subtotal(); ?></span>
+      <?php } ?>
+    </span>
+
+  <?php
+    $fragments['span.cart-content'] = ob_get_clean();
+
+    return $fragments;
+  }
+  add_filter('woocommerce_add_to_cart_fragments', 'bs_mini_cart');
+
+endif;
+// Minicart Header End
+
+
 // WooCommerce Breadcrumb
 if (!function_exists('bs_woocommerce_breadcrumbs')) :
   add_filter('woocommerce_breadcrumb_defaults', 'bs_woocommerce_breadcrumbs');
