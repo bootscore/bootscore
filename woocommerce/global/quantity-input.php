9e59398f7453357c2e41aaed984adbc2b662ff60
Basti <51531217+crftwrk@users.noreply.github.com>
2022-12-20 10:21:44 +0100

    Reformat quantity-input.php

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -22,42 +22,42 @@ $label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 
 
 // In some cases we wish to display the quantity but not allow for it to be changed.
 if ( $max_value && $min_value === $max_value ) {
-	$is_readonly = true;
-	$input_value = $min_value;
+  $is_readonly = true;
+  $input_value = $min_value;
 } else {
-	$is_readonly = false;
+  $is_readonly = false;
 }
 ?>
 <div class="quantity input-group">
-	<?php
-	/**
-	 * Hook to output something before the quantity input field.
-	 *
-	 * @since 7.2.0
-	 */
-	do_action( 'woocommerce_before_quantity_input_field' );
-	?>
+  <?php
+  /**
+   * Hook to output something before the quantity input field.
+   *
+   * @since 7.2.0
+   */
+  do_action( 'woocommerce_before_quantity_input_field' );
+  ?>
   <button type="button" class="input-group-text qty_button minus">-</button>
-	<label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
-	<input
-		type="<?php echo $is_readonly ? 'text' : 'number'; ?>"
-		<?php echo $is_readonly ? 'readonly="readonly"' : ''; ?>
-		id="<?php echo esc_attr( $input_id ); ?>"
-		class="input-text qty text form-control <?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
-		name="<?php echo esc_attr( $input_name ); ?>"
-		value="<?php echo esc_attr( $input_value ); ?>"
-		title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ); ?>"
-		size="4"
-		min="<?php echo esc_attr( $min_value ); ?>"
-		max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
-		<?php if ( ! $is_readonly ): ?>
-			step="<?php echo esc_attr( $step ); ?>"
-			placeholder="<?php echo esc_attr( $placeholder ); ?>"
-			inputmode="<?php echo esc_attr( $inputmode ); ?>"
-			autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
-		<?php endif; ?>
-	/>
-    <button type="button" class="input-group-text qty_button plus">+</button>
+  <label class="screen-reader-text" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
+  <input
+    type="<?php echo $is_readonly ? 'text' : 'number'; ?>"
+    <?php echo $is_readonly ? 'readonly="readonly"' : ''; ?>
+    id="<?php echo esc_attr( $input_id ); ?>"
+    class="input-text qty text form-control <?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
+    name="<?php echo esc_attr( $input_name ); ?>"
+    value="<?php echo esc_attr( $input_value ); ?>"
+    title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'woocommerce' ); ?>"
+    size="4"
+    min="<?php echo esc_attr( $min_value ); ?>"
+    max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+    <?php if ( ! $is_readonly ): ?>
+      step="<?php echo esc_attr( $step ); ?>"
+      placeholder="<?php echo esc_attr( $placeholder ); ?>"
+      inputmode="<?php echo esc_attr( $inputmode ); ?>"
+      autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
+    <?php endif; ?>
+  />
+  <button type="button" class="input-group-text qty_button plus">+</button>
 	<?php
 	/**
 	 * Hook to output something after quantity input field
