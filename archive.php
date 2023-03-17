4e173b24c51a8d3fc8010031457d27b19b3fefad
craftwerk <pst@crftwrk.de>
2023-03-17 10:18:11 +0100

    Remove comments

--- archive.php
+++ archive.php
@@ -14,7 +14,6 @@ get_header();
 <div id="content" class="site-content container py-5 mt-5">
   <div id="primary" class="content-area">
 
-    <!-- Hook to add something nice -->
     <?php bs_after_primary(); ?>
 
     <div class="row">
@@ -22,7 +21,6 @@ get_header();
 
         <main id="main" class="site-main">
 
-          <!-- Title & Description -->
           <header class="page-header">
             <h1><?php the_archive_title(); ?></h1>
             <div class="archive-description lead">
@@ -30,29 +28,29 @@ get_header();
             </div>
           </header>
 
-          <!-- Grid Layout -->
           <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
+              
               <div class="card horizontal mb-4">
                 <div class="row g-0">
-                  <!-- Featured Image-->
+
                   <?php if (has_post_thumbnail())
                     echo '<div class="card-img-left-md col-lg-6 col-xl-5 col-xxl-4"><a href="' . get_the_permalink() . '">' . get_the_post_thumbnail(null, 'medium') . '</a></div>';             
                   ?>
+                  
                   <div class="col">
                     <div class="card-body">
 
                       <?php bootscore_category_badge(); ?>
 
-                      <!-- Title -->
                       <h2 class="blog-post-title h5">
                         <a class="text-body text-decoration-none" href="<?php the_permalink(); ?>">
                           <?php the_title(); ?>
                         </a>
                       </h2>
-                      <!-- Meta -->
+
                       <?php if ('post' === get_post_type()) : ?>
-                        <p class="small mb-2 text-muted">
+                        <p class="meta small mb-2 text-muted">
                           <?php
                             bootscore_date();
                             bootscore_author();
@@ -61,38 +59,37 @@ get_header();
                           ?>
                         </p>
                       <?php endif; ?>
-                      <!-- Excerpt & Read more -->
+
                       <div class="card-text">
                         <a class="text-body text-decoration-none" href="<?php the_permalink(); ?>">
-                        <?php the_excerpt(); ?> 
+                          <?php the_excerpt(); ?> 
                         </a>
                       </div>
-                       <p class="card-text"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a></p>
-     
-                      <!-- Tags -->
+                      
+                      <p class="card-text"><a class="read-more" href="<?php the_permalink(); ?>"><?php _e('Read more »', 'bootscore'); ?></a></p>
+
                       <?php bootscore_tags(); ?>
 
                     </div>
                   </div>
                 </div>
               </div>
+          
             <?php endwhile; ?>
           <?php endif; ?>
 
-          <!-- Pagination -->
           <div>
             <?php bootscore_pagination(); ?>
           </div>
 
-        </main><!-- #main -->
+        </main>
 
-      </div><!-- col -->
-
+      </div>
       <?php get_sidebar(); ?>
-    </div><!-- row -->
+    </div>
 
-  </div><!-- #primary -->
-</div><!-- #content -->
+  </div>
+</div>
 
 <?php
 get_footer();
