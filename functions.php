7bfe5e061520440be15b75b6710c31b724da2bff
craftwerk <pst@crftwrk.de>
2023-04-14 11:10:52 +0200

    Fix sidebar `card-header`

--- functions.php
+++ functions.php
@@ -157,10 +157,10 @@ if (!function_exists('bootscore_widgets_init')) :
       'name'          => esc_html__('Sidebar', 'bootscore'),
       'id'            => 'sidebar-1',
       'description'   => esc_html__('Add widgets here.', 'bootscore'),
-      'before_widget' => '<section id="%1$s" class="widget %2$s card mb-4">',
-      'after_widget'  => '</div></section>',
+      'before_widget' => '<section id="%1$s" class="widget %2$s card card-body mb-4">',
+      'after_widget'  => '</section>',
       'before_title'  => '<h2 class="widget-title card-header h5">',
-      'after_title'   => '</h2><div class="card-body">',
+      'after_title'   => '</h2>',
     ));
 
     // Top Footer
