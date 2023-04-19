ab9b1f7a8a11a070054dcd0533dbdfb7da5a8093
craftwerk <pst@crftwrk.de>
2023-04-19 11:04:08 +0200

    Add col class function to single templates

--- single-templates/single-sidebar-left.php
+++ single-templates/single-sidebar-left.php
@@ -17,7 +17,7 @@ get_header();
 
     <div class="row">
       <?php get_sidebar(); ?>
-      <div class="col-md-8 col-lg-9 order-first order-md-last">
+      <div class="<?php echo bootscore_main_col_class(); ?> order-first order-md-last">
 
         <main id="main" class="site-main">
 
