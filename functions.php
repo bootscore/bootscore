10e793b96c84c8a79480eb880fbf8ae6e6a9085d
craftwerk <pst@crftwrk.de>
2023-05-19 17:37:18 +0200

    Password protected form as input-group

--- functions.php
+++ functions.php
@@ -455,10 +455,9 @@ endif;
 if (!function_exists('bootscore_pw_form')) :
   function bootscore_pw_form() {
     $output = '
-        <form action="' . get_option('siteurl') . '/wp-login.php?action=postpass" method="post" class="form-inline">' . "\n"
-      . '<input name="post_password" type="password" size="" class="form-control me-2 my-1" placeholder="' . __('Password', 'bootscore') . '"/>' . "\n"
-      . '<input type="submit" class="btn btn-outline-primary my-1" name="Submit" value="' . __('Submit', 'bootscore') . '" />' . "\n"
-      . '</p>' . "\n"
+        <form action="' . get_option('siteurl') . '/wp-login.php?action=postpass" method="post" class="input-group">' . "\n"
+      . '<input name="post_password" type="password" size="" class="form-control" placeholder="' . __('Password', 'bootscore') . '"/>' . "\n"
+      . '<input type="submit" class="btn btn-outline-primary input-group-text" name="Submit" value="' . __('Submit', 'bootscore') . '" />' . "\n"
       . '</form>' . "\n";
     return $output;
   }
