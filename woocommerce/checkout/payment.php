c9eec71af3865f0983b0fb92b1dc33af4050ce0f
craftwerk <pst@crftwrk.de>
2023-05-27 12:06:09 +0200

    Update payment.php

--- woocommerce/checkout/payment.php
+++ woocommerce/checkout/payment.php
@@ -13,7 +13,7 @@
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 7.0.1
+ * @version 7.8.0
  */
 
 defined('ABSPATH') || exit;
@@ -32,7 +32,9 @@ if (!is_ajax()) {
             wc_get_template('checkout/payment-method.php', array('gateway' => $gateway));
           }
         } else {
-          echo '<div class="woocommerce-notice woocommerce-notice--info woocommerce-info">' . apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ) . '</div>'; // @codingStandardsIgnoreLine
+            echo '<div>';
+            wc_print_notice( apply_filters( 'woocommerce_no_available_payment_methods_message', WC()->customer->get_billing_country() ? esc_html__( 'Sorry, it seems that there are no available payment methods for your state. Please contact us if you require assistance or wish to make alternate arrangements.', 'woocommerce' ) : esc_html__( 'Please fill in your details above to see available payment methods.', 'woocommerce' ) ), 'notice' ); // phpcs:ignore WooCommerce.Commenting.CommentHooks.MissingHookComment
+            echo '</div>';
         }
         ?>
       </div>
