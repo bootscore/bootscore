758b6d9a5439aef6f271e8baa6a31bbe114e1b09
craftwerk <pst@crftwrk.de>
2023-07-17 10:38:59 +0200

    Update translation template

--- languages/plugin-translations.php
+++ languages/plugin-translations.php
@@ -1,11 +1,21 @@
 
+<!-- bS Isotope -->
 <?php _e('All','bootscore'); ?>
+
+<!-- bS Share Buttons -->
 <?php _e('Look what I found: ','bootscore'); ?>
+
+<!-- bS Cookie Banner -->
 <?php esc_html_e('Cookies help us deliver our services. By using our services, you agree to our use of cookies.', 'bootscore'); ?>
 <?php esc_html_e('/privacy-policy', 'bootscore'); ?>
 <?php esc_html_e('More Information', 'bootscore'); ?>
 <?php esc_html_e('Accept', 'bootscore'); ?>		
+
+<!-- bS Dark Mode -->
 <?php _e('Toggle theme', 'bootscore'); ?>
 <?php _e('Light', 'bootscore'); ?>
 <?php _e('Dark', 'bootscore'); ?>
 <?php _e('Auto', 'bootscore'); ?>
+
+<!-- bS Swiper -->
+<?php _e('You might also like', 'bootscore'); ?>
