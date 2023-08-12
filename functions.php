d01cd86f2c4437bcbb8ebcc3742062238ce2b0d4
craftwerk <pst@crftwrk.de>
2023-08-12 16:50:35 +0200

    Widgets

--- functions.php
+++ functions.php
@@ -8,6 +8,39 @@
  * @package Bootscore
  */
 
+/*
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+*/
+
+
+// Array of files to include.
+$bootscore_includes = array(
+	//'/theme-settings.php',                  // Initialize theme default settings.
+	//'/setup.php',                           // Theme setup and custom theme supports.
+	//'inc/widgets.php',                         // Register widget area.
+	//'/enqueue.php',                         // Enqueue scripts and styles.
+	//'/template-tags.php',                   // Custom template tags for this theme.
+	//'inc/pagination.php',                      // Custom pagination for this theme.
+	//'/hooks.php',                           // Custom hooks.
+	//'/extras.php',                          // Custom functions that act independently of the theme templates.
+	//'/customizer.php',                      // Customizer additions.
+	//'/custom-comments.php',                 // Custom Comments file.
+	//'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker. Trying to get deeper navigation? Check out: https://github.com/understrap/understrap/issues/567.
+	//'/editor.php',                          // Load Editor functions.
+	//'/block-editor.php',                    // Load Block Editor functions.
+	//'/deprecated.php',                      // Load deprecated functions.
+);
+
+
+require_once('inc/widgets.php');
+
+
+
+
+
+
+
 
 // Enable WooCommerce scripts if plugin is installed
 if (class_exists('WooCommerce')) {
@@ -37,6 +70,13 @@ if (!function_exists('register_comment_list')) :
 endif;
 add_action('after_setup_theme', 'register_comment_list');
 // Register Comment List END
+
+
+
+
+
+
+
 
 
 if (!function_exists('bootscore_setup')) :
@@ -113,6 +153,7 @@ add_action('after_setup_theme', 'bootscore_content_width', 0);
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
+/*
 // Widgets
 if (!function_exists('bootscore_widgets_init')) :
 
@@ -246,7 +287,7 @@ if (!function_exists('bootscore_widgets_init')) :
 
 endif;
 // Widgets END
-
+*/
 
 // Shortcode in HTML-Widget
 add_filter('widget_text', 'do_shortcode');
