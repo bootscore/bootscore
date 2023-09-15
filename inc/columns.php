594c872b2cb5beb92bc21643d4245417e0709ea0
Justin Kruit <justinkruitnl@gmail.com>
2023-09-15 19:48:13 +0200

    Remove old functions

--- inc/columns.php
+++ inc/columns.php
@@ -11,23 +11,6 @@
 // Exit if accessed directly
 defined('ABSPATH') || exit;
 
-
-/**
- * Make main content col dynamic if sidebar widgets exists
- * @return string
- */
-if (!function_exists('bootscore_main_col_class')) {
-  function bootscore_main_col_class() {
-    if (!is_active_sidebar('sidebar-1')) {
-      // Sidebar is empty
-      return "col";
-    } else {
-      // Sidebar has widgets
-      return "col-md-8 col-lg-9";
-    }
-  }
-}
-
 /**
  * Determines the CSS class for the main column based on the presence of a sidebar.
  *
@@ -46,35 +29,3 @@ function bootscore_main_col_class_sidebar($string) {
 
 add_filter('bootscore_main_col_class', 'bootscore_main_col_class_sidebar');
 
-
-/**
- * Sidebar column width and breakpoints
- * @return string
- */
-if (!function_exists('bootscore_sidebar_col_class')) {
-  function bootscore_sidebar_col_class() {
-    return "col-md-4 col-lg-3 order-first order-md-last";
-  }
-}
-
-
-/**
- * Sidebar responsive offcanvas toggler
- * @return string
- */
-if (!function_exists('bootscore_sidebar_toggler_class')) {
-  function bootscore_sidebar_toggler_class() {
-    return "d-md-none btn btn-outline-primary w-100 mb-4 d-flex justify-content-between align-items-center";
-  }
-}
-
-
-/**
- * Sidebar responsive offcanvas breakpoint and placement
- * @return string
- */
-if (!function_exists('bootscore_sidebar_offcanvas_class')) {
-  function bootscore_sidebar_offcanvas_class() {
-    return "offcanvas-md offcanvas-end";
-  }
-}
