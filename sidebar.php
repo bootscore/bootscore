6d7e18a53ca2125f068d70cfe2f818d3ccaa0748
Justin Kruit <justinkruitnl@gmail.com>
2023-09-15 13:37:39 +0200

    Replace functions with filters in sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -18,14 +18,14 @@ if (!is_active_sidebar('sidebar-1')) {
   return;
 }
 ?>
-<div class="<?= bootscore_sidebar_col_class(); ?>">
+<div class="<?= apply_filters('bootscore_sidebar_col_class', 'col-md-4 col-lg-3 order-first order-md-last'); ?>">
   <aside id="secondary" class="widget-area">
 
-    <button class="<?= bootscore_sidebar_toggler_class(); ?>" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
+    <button class="<?= apply_filters('bootscore_sidebar_toggler_class', 'd-md-none btn btn-outline-primary w-100 mb-4 d-flex justify-content-between align-items-center') ?>" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
       <?php esc_html_e('Open side menu', 'bootscore'); ?> <i class="fa-solid fa-ellipsis-vertical"></i>
     </button>
 
-    <div class="<?= bootscore_sidebar_offcanvas_class(); ?>" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
+    <div class="<?= apply_filters('bootscore_sidebar_offcanvas_class', 'offcanvas-md offcanvas-end') ?>" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
       <div class="offcanvas-header">
         <span class="h5 offcanvas-title" id="sidebarLabel"><?php esc_html_e('Sidebar', 'bootscore'); ?></span>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebar" aria-label="Close"></button>
