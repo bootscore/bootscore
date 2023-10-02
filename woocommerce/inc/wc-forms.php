f26b41a404b37b5986e4aa10970f809503d57702
craftwerk <pst@crftwrk.de>
2023-10-02 17:49:54 +0200

    Update wc-forms.php

--- woocommerce/inc/wc-forms.php
+++ woocommerce/inc/wc-forms.php
@@ -16,6 +16,7 @@ defined( 'ABSPATH' ) || exit;
  * Remove CSS and/or JS for Select2 used by WooCommerce
  * https://gist.github.com/Willem-Siebe/c6d798ccba249d5bf080.
  */
+
 add_action('wp_enqueue_scripts', 'bootscore_dequeue_stylesandscripts_select2', 100);
 
 function bootscore_dequeue_stylesandscripts_select2() {
@@ -30,16 +31,89 @@ function bootscore_dequeue_stylesandscripts_select2() {
 
 
 
+/*********************************************************************************************/
+/** WooCommerce - Modify each individual input type $args defaults /**
+/*********************************************************************************************/
+
+/*
+add_filter('woocommerce_form_field_args','wc_form_field_args',10,3);
+
+function wc_form_field_args( $args, $key, $value = null ) {
+
+
+// Start field type switch case
+
+switch ( $args['type'] ) {
+
+    case "select" :  // Targets all select input type elements, except the country and state select input types 
+        //$args['class'][] = 'form-group'; // Add a class to the field's html element wrapper - woocommerce input types (fields) are often wrapped within a <p></p> tag  
+        $args['input_class'] = array('form-select'); // Add a class to the form input itself
+        //$args['custom_attributes']['data-plugin'] = 'select2';
+        $args['label_class'] = array('form-label');
+        $args['custom_attributes'] = array( 'data-plugin' => 'select2', 'data-allow-clear' => 'true', 'aria-hidden' => 'true',  ); // Add custom data attributes to the form input itself
+    break;
+
+    case 'country' : // By default WooCommerce will populate a select with the country names - $args defined for this specific input type targets only the country select element
+        $args['input_class'] = array('form-select');    
+        $args['class'][] = 'form-group single-country';
+        $args['label_class'] = array('form-label');
+    break;
+
+    case "state" : // By default WooCommerce will populate a select with state names - $args defined for this specific input type targets only the country select element
+        //$args['class'][] = 'form-group'; // Add class to the field's html element wrapper 
+        $args['input_class'] = array('form-control', 'input-lg'); // add class to the form input itself
+        //$args['custom_attributes']['data-plugin'] = 'select2';
+        $args['label_class'] = array('control-label');
+        $args['custom_attributes'] = array( 'data-plugin' => 'select2', 'data-allow-clear' => 'true', 'aria-hidden' => 'true',  );
+    break;
+
+
+    case "password" :
+    case "text" :
+    case "email" :
+    case "tel" :
+    case "number" :
+        //$args['class'][] = 'form-group';
+        //$args['input_class'][] = 'form-control input-lg'; // will return an array of classes, the same as bellow
+        $args['input_class'] = array('form-control');
+        $args['label_class'] = array('form-label');
+    break;
+
+    case 'textarea' :
+        $args['input_class'] = array('form-control');
+        $args['label_class'] = array('form-label');
+    break;
+
+    case 'checkbox' :  
+    break;
+
+    case 'radio' :
+    break;
+
+    default :
+        //$args['class'][] = 'form-group';
+        $args['input_class'] = array('form-control');
+        $args['label_class'] = array('form-label');
+    break;
+    }
+
+    return $args;
+}
+
+*/
+
+
 
 
 
 // Bootstrap Billing forms
-function iap_wc_bootstrap_form_field_args($args, $key, $value) {
+function bootscore_wc_bootstrap_form_field_args($args, $key, $value) {
 
   $args['input_class'][] = 'form-control';
+  $args['label_class'] = array('form-label');
   return $args;
 }
-add_filter('woocommerce_form_field_args', 'iap_wc_bootstrap_form_field_args', 10, 3);
+add_filter('woocommerce_form_field_args', 'bootscore_wc_bootstrap_form_field_args', 10, 3);
 
 
 
@@ -85,3 +159,5 @@ function woocommerce_quantity_input( $args = array(), $product = null, $echo = t
         return ob_get_clean();
     }
 }
+
+
