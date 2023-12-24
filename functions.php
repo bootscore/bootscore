9709fa93f426507c71091cbf4810ad313ebeff2c
craftwerk <pst@crftwrk.de>
2023-12-24 12:59:51 +0100

    Add support for categories

--- functions.php
+++ functions.php
@@ -33,6 +33,7 @@ require_once('inc/template-functions.php');      // Functions which enhance the 
 require_once('inc/widgets.php');                 // Register widget area and disables Gutenberg in widgets
 require_once('inc/deprecated.php');              // Fallback functions being dropped in v6
 
+require_once('inc/blocks/block-categories.php');    // Archive block
 require_once('inc/blocks/block-search.php');     // Searchform block
 
 
