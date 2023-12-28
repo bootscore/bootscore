6691b735c9df9323af999ead029a47b3f16cdc91
craftwerk <pst@crftwrk.de>
2023-12-28 11:20:11 +0100

    Update block-widget-latest-posts.php

--- inc/blocks/block-widget-latest-posts.php
+++ inc/blocks/block-widget-latest-posts.php
@@ -33,14 +33,16 @@ if (!function_exists('bootscore_block_widget_latest_posts_classes')) {
         '<a',
         'wp-block-latest-posts__post-author',
         'wp-block-latest-posts__post-date',
+        'wp-block-latest-posts__post-excerpt',
       );
       $replace = array(
         'wp-block-latest-posts__list bs-list-group list-group',
         '<li class="list-group-item list-group-item-action"',
-        'wp-post-image rounded',
+        'wp-post-image rounded mb-3',
         '<a class="stretched-link text-decoration-none"',
         'small text-body-secondary',
         'small text-body-secondary d-block',
+        'wp-block-latest-posts__post-excerpt mb-0',
       );
 
       $block_content = str_replace($search, $replace, $block_content);
