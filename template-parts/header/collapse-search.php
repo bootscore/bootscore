8357f84ac272f1be1c664b39e5fcdcd7e6e52014
craftwerk <pst@crftwrk.de>
2024-03-08 09:44:21 +0000

    Fix search collapse breakpoint

--- template-parts/header/collapse-search.php
+++ template-parts/header/collapse-search.php
@@ -18,7 +18,7 @@ defined('ABSPATH') || exit;
 
 <!-- Collapse Search Mobile -->
 <?php if (is_active_sidebar('top-nav-search')) : ?>
-  <div class="collapse <?= apply_filters('bootscore/class/header/collapse', 'bg-body-tertiary position-absolute start-0 end-0 d-lg-none'); ?>" id="collapse-search">
+  <div class="collapse <?= apply_filters('bootscore/class/header/collapse', 'bg-body-tertiary position-absolute start-0 end-0'); ?> d-lg-none" id="collapse-search">
     <div class="<?= apply_filters('bootscore/class/container', 'container', 'collapse-search'); ?> pb-2">
       <?php dynamic_sidebar('top-nav-search'); ?>
     </div>
