a3d3bf9bd0ac51f4f05bd284b39bde11ac9cb0b6
craftwerk <pst@crftwrk.de>
2024-03-19 11:10:32 +0100

    Remove empty IE warning function

--- inc/deprecated.php
+++ inc/deprecated.php
@@ -12,20 +12,7 @@
 defined('ABSPATH') || exit;
 
 
-// Remove in v6
-// Internet Explorer Warning Alert
-if (!function_exists('bootscore_ie_alert')) :
-  /**
-   * Deprecated - functionality is removed already - Code will be removed in a future release.
-   * Replaced with a js solution to prevent page caching
-   *
-   * (Displays an alert if page is browsed by Internet Explorer)
-   *
-   * function stays to not break child themes with the function bootscore_ie_alert() immediately
-   */
-  function bootscore_ie_alert() {
-  }
-endif;
+
 
 
 
