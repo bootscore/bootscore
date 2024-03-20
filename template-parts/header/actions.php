f005a53b445f934c89cf85415628dfea1f8dd6e8
craftwerk <pst@crftwrk.de>
2024-03-20 11:27:04 +0100

    Add unique class to each header button

--- template-parts/header/actions.php
+++ template-parts/header/actions.php
@@ -18,14 +18,14 @@ defined('ABSPATH') || exit;
 
 <!-- Searchform large -->
 <?php if (is_active_sidebar('top-nav-search')) : ?>
-  <div class="d-none d-lg-block ms-1 ms-md-2 top-nav-search-lg">
+  <div class="d-none d-lg-block ms-1 ms-md-2 nav-search-lg">
     <?php dynamic_sidebar('top-nav-search'); ?>
   </div>
 <?php endif; ?>
 
 <!-- Search toggler mobile -->
 <?php if (is_active_sidebar('top-nav-search')) : ?>
-  <button class="<?= apply_filters('bootscore/class/header_button', 'btn btn-outline-secondary', 'search'); ?> d-lg-none ms-1 ms-md-2 top-nav-search-md" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-search" aria-expanded="false" aria-controls="collapse-search">
+  <button class="<?= apply_filters('bootscore/class/header_button', 'btn btn-outline-secondary', 'search'); ?> d-lg-none ms-1 ms-md-2 search-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-search" aria-expanded="false" aria-controls="collapse-search">
     <i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span>
   </button>
 <?php endif; ?>
