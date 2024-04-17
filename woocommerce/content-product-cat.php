4a4cdbb0c26d099298c7181af9bc0688306c82bc
craftwerk <pst@crftwrk.de>
2024-04-17 10:43:47 +0200

    Rename Woo grid filters

--- woocommerce/content-product-cat.php
+++ woocommerce/content-product-cat.php
@@ -20,7 +20,7 @@ if (!defined('ABSPATH')) {
   exit;
 }
 ?>
-<div <?php wc_product_cat_class(apply_filters('bootscore/class/col/woocommerce', 'col-md-6 col-lg-4 col-xxl-3'), $category); ?>>
+<div <?php wc_product_cat_class(apply_filters('bootscore/class/woocommerce/col', 'col-md-6 col-lg-4 col-xxl-3'), $category); ?>>
   <div class="card h-100">
     <?php
     /**
