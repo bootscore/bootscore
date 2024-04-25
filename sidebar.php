5f516cac2df3db448ecbbdcc2fd3f5b28350bbde
Basti <51531217+crftwrk@users.noreply.github.com>
2024-04-25 11:47:47 +0200

    Fix missing `;` in offcanvas filter

--- sidebar.php
+++ sidebar.php
@@ -25,7 +25,7 @@ if (!is_active_sidebar('sidebar-1')) {
       <?php esc_html_e('Open side menu', 'bootscore'); ?> <i class="fa-solid fa-ellipsis-vertical"></i>
     </button>
 
-    <div class="<?= apply_filters('bootscore/class/sidebar/offcanvas', 'offcanvas-lg offcanvas-end') ?>" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
+    <div class="<?= apply_filters('bootscore/class/sidebar/offcanvas', 'offcanvas-lg offcanvas-end'); ?>" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
       <div class="offcanvas-header">
         <span class="h5 offcanvas-title" id="sidebarLabel"><?php esc_html_e('Sidebar', 'bootscore'); ?></span>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebar" aria-label="Close"></button>
