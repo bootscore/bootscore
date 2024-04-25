d6f5bdcf6384e026ea489b858015196d3e215ab9
Basti <51531217+crftwrk@users.noreply.github.com>
2024-04-25 11:50:00 +0200

    Fix missing `;` in filter

--- sidebar.php
+++ sidebar.php
@@ -21,7 +21,7 @@ if (!is_active_sidebar('sidebar-1')) {
 <div class="<?= apply_filters('bootscore/class/sidebar/col', 'col-lg-3 order-first order-lg-2'); ?>">
   <aside id="secondary" class="widget-area">
 
-    <button class="<?= apply_filters('bootscore/class/sidebar/button', 'd-lg-none btn btn-outline-primary w-100 mb-4 d-flex justify-content-between align-items-center') ?>" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
+    <button class="<?= apply_filters('bootscore/class/sidebar/button', 'd-lg-none btn btn-outline-primary w-100 mb-4 d-flex justify-content-between align-items-center'); ?>" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
       <?php esc_html_e('Open side menu', 'bootscore'); ?> <i class="fa-solid fa-ellipsis-vertical"></i>
     </button>
 
