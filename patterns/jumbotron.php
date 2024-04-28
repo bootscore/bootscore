fe50364f6fcd491ba26b16319160101aaee9b7a4
craftwerk <pst@crftwrk.de>
2024-04-28 22:14:38 +0200

    Create jumbotron.php

---
+++ patterns/jumbotron.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: jumbotron
+ * Slug: bootscore/jumbotron
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"className":"p-5 text-center bg-body-tertiary rounded-3","layout":{"type":"default"}} -->
+<div class="wp-block-group p-5 text-center bg-body-tertiary rounded-3 mb-3"><!-- wp:heading {"level":1,"className":"text-body-emphasis"} -->
+<h1 class="wp-block-heading text-body-emphasis">Basic jumbotron</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead mb-0"} -->
+<p class="lead mb-0">This is a simple Bootstrap jumbotron that sits within a&nbsp;<code>.container</code>, recreated with built-in utility classes.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
