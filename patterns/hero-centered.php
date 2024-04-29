8406a9512cd613feecccd53c2de30079c9ddb6aa
craftwerk <pst@crftwrk.de>
2024-04-29 13:50:28 +0200

    Create hero patterns

---
+++ patterns/hero-centered.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: hero centered
+ * Slug: bootscore/hero-centered
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"className":"bg-body-tertiary py-5 mb-3","layout":{"type":"default"}} -->
+<div class="wp-block-group bg-body-tertiary py-5 mb-3"><!-- wp:group {"className":"container h-100 d-flex justify-content-center align-items-center","layout":{"type":"default"}} -->
+<div class="wp-block-group container h-100 d-flex justify-content-center align-items-center"><!-- wp:group {"className":"text-center","layout":{"type":"default"}} -->
+<div class="wp-block-group text-center"><!-- wp:heading {"className":"display-5 fw-bold"} -->
+<h2 class="wp-block-heading display-5 fw-bold">Hero centered</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">A simple centered hero pattern. Use <code>height-75 width-100</code> classes on the outer group to stretch it through the <code>container</code>. This may disturb layout in the backend.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a class="btn btn-lg btn-primary" href="#">Button</a> <a class="btn btn-lg btn-secondary" href="#">Button</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/hero-left-aligned.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Title: hero left aligned
+ * Slug: bootscore/hero-left-aligned
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"className":"bg-body-tertiary py-5 mb-3","layout":{"type":"default"}} -->
+<div class="wp-block-group bg-body-tertiary py-5 mb-3"><!-- wp:group {"className":"container h-100","layout":{"type":"default"}} -->
+<div class="wp-block-group container h-100"><!-- wp:group {"className":"row h-100","layout":{"type":"default"}} -->
+<div class="wp-block-group row h-100"><!-- wp:group {"className":"col-lg-6","layout":{"type":"default"}} -->
+<div class="wp-block-group col-lg-6"><!-- wp:group {"className":"h-100 d-flex align-items-center","layout":{"type":"default"}} -->
+<div class="wp-block-group h-100 d-flex align-items-center"><!-- wp:group {"layout":{"type":"default"}} -->
+<div class="wp-block-group"><!-- wp:heading {"className":"display-5 fw-bold"} -->
+<h2 class="wp-block-heading display-5 fw-bold">Hero left aligned</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead"} -->
+<p class="lead">A simple centered hero pattern.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a class="btn btn-lg btn-primary" href="#">Button</a> <a class="btn btn-lg btn-secondary" href="#">Button</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"col-lg-6 mb-3 mb-lg-0 d-flex align-items-center order-first order-lg-last","layout":{"type":"default"}} -->
+<div class="wp-block-group col-lg-6 mb-3 mb-lg-0 d-flex align-items-center order-first order-lg-last"><!-- wp:html -->
+<img class="rounded" src="https://dummyimage.com/1200x900/6c757d/ffffff">
+<!-- /wp:html --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
