16bfd414cb8fb0f64f751d237764db4ef7707b60
craftwerk <pst@crftwrk.de>
2024-04-30 11:27:05 +0200

    Added container pattern

---
+++ patterns/container-section.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: container section
+ * Slug: bootscore/container-section
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"tagName":"section","className":"py-5 bg-body-tertiary","layout":{"type":"default"}} -->
+<section class="wp-block-group py-5 bg-body-tertiary"><!-- wp:group {"className":"container","layout":{"type":"default"}} -->
+<div class="wp-block-group container"><!-- wp:paragraph -->
+<p><code>section</code> with <code>container</code></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group -->
---
+++ patterns/container.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: container
+ * Slug: bootscore/container
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"className":"container","layout":{"type":"default"}} -->
+<div class="wp-block-group container"><!-- wp:paragraph -->
+<p><code>container</code></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
