f874cf2b28234d4a33169888fc84615fdc66a69f
craftwerk <pst@crftwrk.de>
2024-05-02 10:23:12 +0200

    Added section with heading and pricing pattern

---
+++ patterns/pricing.php
@@ -0,0 +1,124 @@
+<?php
+/**
+ * Title: pricing
+ * Slug: bootscore/pricing
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"className":"row","layout":{"type":"default"}} -->
+<div class="wp-block-group row"><!-- wp:group {"className":"col-md-4 mb-3 mb-md-0","layout":{"type":"default"}} -->
+<div class="wp-block-group col-md-4 mb-3 mb-md-0"><!-- wp:group {"className":"card text-center"} -->
+<div class="wp-block-group card text-center"><!-- wp:heading {"className":"card-header h4"} -->
+<h2 class="wp-block-heading card-header h4">Free</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"card-body","layout":{"type":"default"}} -->
+<div class="wp-block-group card-body"><!-- wp:heading {"level":3,"className":"card-title"} -->
+<h3 class="wp-block-heading card-title">$0<small class="text-body-secondary">/mo</small></h3>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"list-unstyled mt-3 mb-4"} -->
+<ul class="list-unstyled mt-3 mb-4"><!-- wp:list-item -->
+<li>10 users included</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>2 GB of storage</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Email support</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Help center access</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><a class="btn btn-lg  btn-outline-primary d-block" href="#">Sign up for free</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"col-md-4 mb-3 mb-md-0","layout":{"type":"default"}} -->
+<div class="wp-block-group col-md-4 mb-3 mb-md-0"><!-- wp:group {"className":"card text-center"} -->
+<div class="wp-block-group card text-center"><!-- wp:heading {"className":"card-header h4"} -->
+<h2 class="wp-block-heading card-header h4">Pro</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"card-body","layout":{"type":"default"}} -->
+<div class="wp-block-group card-body"><!-- wp:heading {"level":3,"className":"card-title"} -->
+<h3 class="wp-block-heading card-title">$15<small class="text-body-secondary">/mo</small></h3>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"list-unstyled mt-3 mb-4"} -->
+<ul class="list-unstyled mt-3 mb-4"><!-- wp:list-item -->
+<li>20 users included</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>10 GB of storage</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Priority email support</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Help center access</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><a class="btn btn-lg  btn-primary d-block" href="#">Get started</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"col-md-4","layout":{"type":"default"}} -->
+<div class="wp-block-group col-md-4"><!-- wp:group {"className":"card text-center border-primary"} -->
+<div class="wp-block-group card text-center border-primary"><!-- wp:heading {"className":"card-header h4 text-bg-primary border-primary"} -->
+<h2 class="wp-block-heading card-header h4 text-bg-primary border-primary">Enterprise</h2>
+<!-- /wp:heading -->
+
+<!-- wp:group {"className":"card-body","layout":{"type":"default"}} -->
+<div class="wp-block-group card-body"><!-- wp:heading {"level":3,"className":"card-title"} -->
+<h3 class="wp-block-heading card-title">$29<small class="text-body-secondary">/mo</small></h3>
+<!-- /wp:heading -->
+
+<!-- wp:list {"className":"list-unstyled mt-3 mb-4"} -->
+<ul class="list-unstyled mt-3 mb-4"><!-- wp:list-item -->
+<li>30 users included</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>15 GB of storage</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Phone and email support</li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li>Help center access</li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p><a class="btn btn-lg  btn-primary d-block" href="https://themes.bootscore.me/boilerplate/contact/">Contact us</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ patterns/section-with-heading.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Title: section with heading
+ * Slug: bootscore/section-with-heading
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"tagName":"section","className":"py-5 bg-body-tertiary","layout":{"type":"default"}} -->
+<section class="wp-block-group py-5 bg-body-tertiary"><!-- wp:group {"className":"container","layout":{"type":"default"}} -->
+<div class="wp-block-group container"><!-- wp:heading {"className":"text-center"} -->
+<h2 class="wp-block-heading text-center">Section with heading</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":"lead text-center mb-4"} -->
+<p class="lead text-center mb-4">This is a <code>&lt;section></code> with heading and subtitle. Content is wrapped in a <code>container</code>.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></section>
+<!-- /wp:group -->
