ce3419d568f3c33c48836080f879367c5f3d3fde
craftwerk <pst@crftwrk.de>
2024-05-05 11:03:29 +0200

    Update from main

--- woocommerce/inc/wc-cart.php
+++ woocommerce/inc/wc-cart.php
@@ -1,30 +1,24 @@
 <?php
 
+ /**
+  * WooCommerce Cart Page
+  *
+  * @package Bootscore 
+  * @version 6.0.0
+  */
+
+
+ // Exit if accessed directly
+ defined('ABSPATH') || exit;
+
+
 /**
- * WooCommerce Enabled Cart Page
- *
- * @package Bootscore 
- * @version 6.0.0
+ * Cross sell products column class
  */
-
-
-// Exit if accessed directly
-defined('ABSPATH') || exit;
-
-
-/**
- * Add class to WooCommerce Mini-Cart "View Cart" and Checkout" buttons
- */
-remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_button_view_cart', 10);
-add_action('woocommerce_widget_shopping_cart_buttons', 'bootscore_widget_shopping_cart_button_view_cart', 10);
-
-function bootscore_widget_shopping_cart_button_view_cart() {
-  echo '<a href="' . esc_url(wc_get_cart_url()) . '" class="btn btn-secondary d-block mb-2">' . esc_html__('View cart', 'woocommerce') . '</a>';
+function bootscore_cart_cross_sell_products($class) {
+  if ( is_cart() ) {
+    return 'col-6';
+  }
+  return $class;
 }
-
-remove_action('woocommerce_widget_shopping_cart_buttons', 'woocommerce_widget_shopping_cart_proceed_to_checkout', 20);
-add_action('woocommerce_widget_shopping_cart_buttons', 'bootscore_widget_shopping_cart_proceed_to_checkout', 20);
-
-function bootscore_widget_shopping_cart_proceed_to_checkout() {
-  echo '<a href="' . esc_url(wc_get_checkout_url()) . '" class="btn btn-primary d-block">' . esc_html__('Checkout', 'woocommerce') . '</a>';
-}
+add_filter('bootscore/class/woocommerce/col', 'bootscore_cart_cross_sell_products', 10, 2);
