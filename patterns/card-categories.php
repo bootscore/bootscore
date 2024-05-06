9763d86172c9a1b7d0d3ce6963f7e1236db6809c
craftwerk <pst@crftwrk.de>
2024-05-06 12:25:44 +0200

    Create card-categories.php

---
+++ patterns/card-categories.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: card categories
+ * Slug: bootscore/card-categories
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.0.0
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"className":"card mb-3"} -->
+<div class="wp-block-group card mb-3"><!-- wp:heading {"className":"card-header h6"} -->
+<h2 class="wp-block-heading card-header h6">Card with <code>list-group-flush</code></h2>
+<!-- /wp:heading -->
+
+<!-- wp:categories {"showPostCounts":true,"className":"list-group-flush"} /--></div>
+<!-- /wp:group -->
