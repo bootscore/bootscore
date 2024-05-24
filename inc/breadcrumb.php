9467373359b16fafd9bad95d58f0747502fc6107
stan1781 <32152442+stan1781@users.noreply.github.com>
2024-05-24 10:41:27 +0800

    Update breadcrumb.php
    Add hidden text “Home” for the breadcrumb’s home label.

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -21,7 +21,7 @@ if (!function_exists('the_breadcrumb')) :
     if (!is_home()) {
       echo '<nav aria-label="breadcrumb" class="overflow-x-auto text-nowrap mb-4 mt-2 py-2 px-3 bg-body-tertiary rounded">';
       echo '<ol class="breadcrumb flex-nowrap mb-0">';
-      echo '<li class="breadcrumb-item"><a href="' . home_url() . '">' . '<i class="fa-solid fa-house"></i>' . '</a></li>';
+      echo '<li class="breadcrumb-item"><a href="' . home_url() . '">' . '<i class="fa-solid fa-house"></i><span class="visually-hidden">' . __('Home', 'bootscore') . '</span>' . '</a></li>';
       // display parent category names with links
       if (is_category() || is_single()) {
         $cat_IDs = wp_get_post_categories(get_the_ID());
