71911edc8608fcead8aeafad852315831894aba3
craftwerk <pst@crftwrk.de>
2024-06-05 16:09:18 +0200

    Fixes the btn class in search if icon is selected

--- inc/blocks/block-widget-search.php
+++ inc/blocks/block-widget-search.php
@@ -29,7 +29,8 @@ if (!function_exists('bootscore_block_widget_search_classes')) {
       '<form ',
       'wp-block-search__input ',
       'wp-block-search__input"',
-      'wp-block-search__button ',
+      'wp-block-search__button wp-element-button',
+      'wp-block-search__button has-icon wp-element-button',
       '<svg class="search-icon" viewBox="0 0 24 24" width="24" height="24">
 					<path d="M13 5c-3.3 0-6 2.7-6 6 0 1.4.5 2.7 1.3 3.7l-3.8 3.8 1.1 1.1 3.8-3.8c1 .8 2.3 1.3 3.7 1.3 3.3 0 6-2.7 6-6S16.3 5 13 5zm0 10.5c-2.5 0-4.5-2-4.5-4.5s2-4.5 4.5-4.5 4.5 2 4.5 4.5-2 4.5-4.5 4.5z"></path>
 				</svg>'
@@ -39,6 +40,7 @@ if (!function_exists('bootscore_block_widget_search_classes')) {
       'wp-block-search__input form-control ',
       'wp-block-search__input form-control"',
       'wp-block-search__button btn btn-outline-secondary ',
+      'wp-block-search__button btn btn-outline-secondary',
       '<i class="fa-solid fa-magnifying-glass"></i>'
     );
 
