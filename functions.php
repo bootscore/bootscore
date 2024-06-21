2408ee6551a71b2840d86fbaa637876ef19f5f97
craftwerk <pst@crftwrk.de>
2024-06-21 20:30:17 +0200

    Include auto updater

--- functions.php
+++ functions.php
@@ -58,3 +58,20 @@ if (class_exists('WooCommerce')) {
 if (defined('JETPACK__VERSION')) {
   require get_template_directory() . '/inc/jetpack.php';
 }
+
+
+/**
+ * Init the update checker
+ * https://github.com/YahnisElsts/plugin-update-checker
+ */
+require 'inc/update/plugin-update-checker.php';
+use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
+
+$myUpdateChecker = PucFactory::buildUpdateChecker(
+	'https://github.com/bootscore/bootscore/',
+	__FILE__,
+	'bootscore'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
