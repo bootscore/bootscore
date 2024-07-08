1382feee12c91fc6e7d45a70384454662d0bfe02
craftwerk <pst@crftwrk.de>
2024-07-08 12:02:47 +0200

    Compile editor.scss if main css has been compiled

--- inc/scss-compiler.php
+++ inc/scss-compiler.php
@@ -213,17 +213,19 @@ function bootscore_child_has_scss() {
 
 function bootscore_compile_scss() {
   // Compile the main.scss file
-  $scss_compiler = new BootscoreScssCompiler();
-  $scss_compiler->scssFile('/assets/scss/main.scss')
-                ->cssFile('/assets/css/main.css')
-                ->addModifiedCheckTheme()
-                ->addModifiedCheck(get_template_directory() . '/assets/scss/bootstrap/bootstrap.scss', false)
-                ->compile();
+  $scss_compiler_main = new BootscoreScssCompiler();
+  $scss_compiler_main ->scssFile('/assets/scss/main.scss')
+                      ->cssFile('/assets/css/main.css')
+                      ->addModifiedCheckTheme()
+                      ->addModifiedCheck(get_template_directory() . '/assets/scss/bootstrap/bootstrap.scss', false)
+                      ->compile();
   
   // Compile the editor.scss file
   $scss_compiler_editor = new BootscoreScssCompiler();
-  $scss_compiler_editor->scssFile('/assets/scss/editor.scss')
-                      ->cssFile('/assets/css/editor.css')
-                      ->addModifiedCheck('/assets/scss/editor.scss')
-                      ->compile();  
+  $scss_compiler_editor ->scssFile('/assets/scss/editor.scss')
+                        ->cssFile('/assets/css/editor.css')
+                        //->addModifiedCheck('/assets/scss/editor.scss')
+                        ->addModifiedCheckTheme()
+                        ->addModifiedCheck(get_stylesheet_directory() . '/assets/scss/main.css', false)
+                        ->compile();
 }
