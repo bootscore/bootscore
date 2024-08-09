eb9e52d176c8cccdc683a92a97b5d105ed772d3d
craftwerk <pst@crftwrk.de>
2024-08-09 17:01:48 +0200

    Add Home text

--- woocommerce/inc/wc-breadcrumb.php
+++ woocommerce/inc/wc-breadcrumb.php
@@ -26,9 +26,7 @@ if (!function_exists('bs_woocommerce_breadcrumbs')) :
       </nav>',
       'before'      => '<li class="breadcrumb-item">',
       'after'       => '</li>',
-      // Remove "Home" and add Fontawesome house icon (_wc_breadcrumb.scss)
-      //'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
-      'home'        => ' ',
+      'home'        => _x('Home', 'breadcrumb', 'woocommerce'),
     );
   }
 endif;
