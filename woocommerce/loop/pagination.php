779ad5672cc4581b17983dd99c2ffd0f7a8af135
craftwerk <pst@crftwrk.de>
2024-08-28 11:27:19 +0200

    Update pagination.php

--- woocommerce/loop/pagination.php
+++ woocommerce/loop/pagination.php
@@ -12,7 +12,7 @@
  *
  * @see     https://woo.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.3.1
+ * @version 9.3.0
  */
 
 if (!defined('ABSPATH')) {
@@ -28,7 +28,7 @@ if ($total <= 1) {
   return;
 }
 ?>
-<nav aria-label="wc-navigation">
+<nav aria-label="<?php esc_attr_e( 'Product Pagination', 'woocommerce' ); ?>">
   <?php
   $paginate_links =  paginate_links(
     apply_filters(
