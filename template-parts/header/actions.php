3c0b94f96e57ce73017576aa4e22e9cd1da06148
craftwerk <pst@crftwrk.de>
2024-09-20 11:14:35 +0200

    Added filter to change search breakpoint if WC is not installed

--- template-parts/header/actions.php
+++ template-parts/header/actions.php
@@ -6,7 +6,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Bootscore
- * @version 6.0.0
+ * @version 6.1.0
  */
 
 
@@ -18,14 +18,14 @@ defined('ABSPATH') || exit;
 
 <!-- Searchform large -->
 <?php if (is_active_sidebar('top-nav-search')) : ?>
-  <div class="d-none d-lg-block ms-1 ms-md-2 nav-search-lg">
+  <div class="d-none d-<?= apply_filters('bootscore/class/header/search/breakpoint', 'lg'); ?>-block ms-1 ms-md-2 nav-search-lg">
     <?php dynamic_sidebar('top-nav-search'); ?>
   </div>
 <?php endif; ?>
 
 <!-- Search toggler mobile -->
 <?php if (is_active_sidebar('top-nav-search')) : ?>
-  <button class="<?= apply_filters('bootscore/class/header/button', 'btn btn-outline-secondary', 'search-toggler'); ?> d-lg-none ms-1 ms-md-2 search-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-search" aria-expanded="false" aria-controls="collapse-search">
+  <button class="<?= apply_filters('bootscore/class/header/button', 'btn btn-outline-secondary', 'search-toggler'); ?> d-<?= apply_filters('bootscore/class/header/search/breakpoint', 'lg'); ?>-none ms-1 ms-md-2 search-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-search" aria-expanded="false" aria-controls="collapse-search">
     <i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span>
   </button>
 <?php endif; ?>
--- template-parts/header/collapse-search.php
+++ template-parts/header/collapse-search.php
@@ -6,7 +6,7 @@
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package Bootscore
- * @version 6.0.0
+ * @version 6.1.0
  */
 
 
@@ -18,7 +18,7 @@ defined('ABSPATH') || exit;
 
 <!-- Collapse Search Mobile -->
 <?php if (is_active_sidebar('top-nav-search')) : ?>
-  <div class="collapse <?= apply_filters('bootscore/class/header/collapse', 'bg-body-tertiary position-absolute start-0 end-0'); ?> d-lg-none" id="collapse-search">
+  <div class="collapse <?= apply_filters('bootscore/class/header/collapse', 'bg-body-tertiary position-absolute start-0 end-0'); ?> d-<?= apply_filters('bootscore/class/header/search/breakpoint', 'lg'); ?>-none" id="collapse-search">
     <div class="<?= apply_filters('bootscore/class/container', 'container', 'collapse-search'); ?> pb-2">
       <?php dynamic_sidebar('top-nav-search'); ?>
     </div>
