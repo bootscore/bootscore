845b1dc91066956b8b7d906e718bd5ca8c8896a4
craftwerk <pst@crftwrk.de>
2024-09-25 11:03:04 +0200

    Added a filter to sidebar title

--- sidebar.php
+++ sidebar.php
@@ -22,12 +22,12 @@ if (!is_active_sidebar('sidebar-1')) {
   <aside id="secondary" class="widget-area">
 
     <button class="<?= apply_filters('bootscore/class/sidebar/button', 'd-lg-none btn btn-outline-primary w-100 mb-4 d-flex justify-content-between align-items-center'); ?>" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
-      <?= apply_filters('bootscore/offcanvas/sidebar/title', __('Open side menu', 'bootscore')); ?> <?= apply_filters('bootscore/icon/ellipsis-vertical', '<i class="fa-solid fa-ellipsis-vertical"></i>'); ?>
+      <?= apply_filters('bootscore/offcanvas/sidebar/button/text', __('Open side menu', 'bootscore')); ?> <?= apply_filters('bootscore/icon/ellipsis-vertical', '<i class="fa-solid fa-ellipsis-vertical"></i>'); ?>
     </button>
 
     <div class="<?= apply_filters('bootscore/class/sidebar/offcanvas', 'offcanvas-lg offcanvas-end'); ?>" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
       <div class="offcanvas-header">
-        <span class="h5 offcanvas-title" id="sidebarLabel"><?php esc_html_e('Sidebar', 'bootscore'); ?></span>
+        <span class="h5 offcanvas-title" id="sidebarLabel"><?= apply_filters('bootscore/offcanvas/sidebar/title', __('Sidebar', 'bootscore')); ?></span>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebar" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body flex-column">
