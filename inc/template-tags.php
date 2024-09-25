3bc56db85ab0adc46efb3097d2d41da708593665
craftwerk <pst@crftwrk.de>
2024-09-25 13:13:50 +0200

    Add filter to comments icon

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -6,7 +6,7 @@
  * Eventually, some of the functionality here could be replaced by core features.
  *
  * @package Bootscore
- * @version 6.0.0
+ * @version 6.1.0
  */
 
 
@@ -133,7 +133,7 @@ if (!function_exists('bootscore_comments')) :
   function bootscore_comments() {
 
     if (!is_single() && !post_password_required() && (comments_open() || get_comments_number())) {
-      echo ' <span class="comment-divider">|</span> <i class="fa-regular fa-comments"></i> <span class="comments-link">';
+      echo ' <span class="comment-divider">|</span> ' . apply_filters('bootscore/icon/comments', '<i class="fa-regular fa-comments"></i>') . ' <span class="comments-link">';
       comments_popup_link(
         sprintf(
           wp_kses(
@@ -192,7 +192,7 @@ if (!function_exists('bootscore_comment_count')) :
    */
   function bootscore_comment_count() {
     if (!post_password_required() && (comments_open() || get_comments_number())) {
-      echo ' <span class="comment-divider">|</span> <i class="fa-regular fa-comments"></i> <span class="comments-link">';
+      echo ' <span class="comment-divider">|</span> ' . apply_filters('bootscore/icon/comments', '<i class="fa-regular fa-comments"></i>') . ' <span class="comments-link">';
 
       /* translators: %s: Name of current post. Only visible to screen readers. */
       // comments_popup_link( sprintf( __( 'Leave a comment<span class="screen-reader-text"> on %s</span>', 'bootscore' ), get_the_title() ) );
