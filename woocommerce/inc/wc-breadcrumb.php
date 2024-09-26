bfd325717c896cfbd346644bbebbb9a71e9c6b69
craftwerk <pst@crftwrk.de>
2024-09-26 16:32:56 +0200

    Added class wc-breadcrumb and styled active item via SCSS

--- woocommerce/inc/wc-breadcrumb.php
+++ woocommerce/inc/wc-breadcrumb.php
@@ -20,7 +20,7 @@ if (!function_exists('bs_woocommerce_breadcrumbs')) :
   function bs_woocommerce_breadcrumbs() {
     return array(
       'delimiter'   => '',
-      'wrap_before' => "<nav aria-label='breadcrumb' class='" . apply_filters('bootscore/class/breadcrumb/nav', 'overflow-x-auto text-nowrap mb-4 mt-2 py-2 px-3 bg-body-tertiary rounded') . "'>
+      'wrap_before' => "<nav aria-label='breadcrumb' class='wc-breadcrumb " . apply_filters('bootscore/class/breadcrumb/nav', 'overflow-x-auto text-nowrap mb-4 mt-2 py-2 px-3 bg-body-tertiary rounded') . "'>
       <ol class='breadcrumb " . apply_filters('bootscore/class/breadcrumb/ol', 'flex-nowrap mb-0') . "'>
       <li class='breadcrumb-item'><a class='" . apply_filters('bootscore/class/breadcrumb/item/link', '') . "' href='" . esc_url(home_url()) . "'>" . apply_filters('bootscore/icon/home', '<i class="fa-solid fa-house"></i>') . " <span class='visually-hidden'>" . __('Home', 'bootscore') . "</span></a></li>", // Use the filter for home icon
       'wrap_after'  => '</ol></nav>',
