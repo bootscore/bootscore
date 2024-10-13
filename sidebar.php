c075120b1401a7a122651c4bd4104cad3f505902
craftwerk <pst@crftwrk.de>
2024-10-13 10:52:48 +0200

    Added hooks to sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -31,7 +31,13 @@ if (!is_active_sidebar('sidebar-1')) {
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" data-bs-target="#sidebar" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body flex-column">
+        
+        <?php do_action( 'bootscore_before_sidebar_widgets' ); ?>
+        
         <?php dynamic_sidebar('sidebar-1'); ?>
+        
+        <?php do_action( 'bootscore_after_sidebar_widgets' ); ?>
+        
       </div>
     </div>
 
