c9051f1214fe6232deb57dbe692d2019da75276e
craftwerk <pst@crftwrk.de>
2024-12-20 12:25:10 +0100

    Update wc-forms.php

--- woocommerce/inc/wc-forms.php
+++ woocommerce/inc/wc-forms.php
@@ -49,11 +49,55 @@ function bootscore_quantity_input_classes( $classes ) {
 add_filter( 'woocommerce_quantity_input_classes', 'bootscore_quantity_input_classes' );
 
 
+
+
+
+
+// Reviews
+
+
+/**
+ * Change the 'comments' div ID to 'woo-comments'
+ */
+add_filter('comments_template', function ($template) {
+    // Include the default comments template, and capture the output.
+    ob_start();
+    include($template);
+    $output = ob_get_clean();
+
+    // Replace the 'comments' div ID with 'woo-comments'
+    $output = preg_replace('/<div id="comments"/', '<div id="woo-comments"', $output, 1);
+
+    // Return the modified output
+    echo $output;
+    return '';
+}, 20);
+
+
+
+
+/**
+ * Change reply title to h3 in WooCommerce reviews
+ */
+add_filter('woocommerce_product_review_comment_form_args', function ($args) {
+  if (!empty($args['title_reply'])) {
+    // Replace the reply title span with h3
+    $args['title_reply_before'] = '<h3 id="reply-title" class="comment-reply-title">';
+    $args['title_reply_after'] = '</h3>';
+  }
+
+  return $args;
+});
+
+
 /**
  * Reviews text area
  */
 add_filter('comment_form_field_comment', function ($field) {
-  // Add the 'form-control' class to the textarea
+  // Remove the label
+  $field = preg_replace('/<label[^>]*for="comment"[^>]*>.*?<\/label>/i', '', $field);
+
+  // Add the 'form-control' class and placeholder to the textarea
   $field = str_replace(
     '<textarea',
     '<textarea class="form-control" placeholder="' . __('Your review...*', 'bootscore') . '"',
@@ -62,3 +106,4 @@ add_filter('comment_form_field_comment', function ($field) {
 
   return $field;
 });
+
