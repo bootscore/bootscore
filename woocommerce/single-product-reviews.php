ea06c8dd293cc6d132c2813888e5ac4efd2fdaf4
craftwerk <pst@crftwrk.de>
2025-01-03 16:49:56 +0100

    Updated the scss for the reviews

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -41,7 +41,7 @@ if (!comments_open()) {
     </h2>
 
     <?php if (have_comments()) : ?>
-      <ol class="list-unstyled">
+      <ol class="comment-list">
         <?php wp_list_comments(apply_filters('woocommerce_product_review_list_args', array('callback' => 'woocommerce_comments'))); ?>
       </ol>
 
