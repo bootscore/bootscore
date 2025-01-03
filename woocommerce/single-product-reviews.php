701a2dea8e1d67fb9be15b19d49e22671d342176
craftwerk <pst@crftwrk.de>
2025-01-03 11:58:01 +0100

    Remove unneeded commentlist class

--- woocommerce/single-product-reviews.php
+++ woocommerce/single-product-reviews.php
@@ -41,7 +41,7 @@ if (!comments_open()) {
     </h2>
 
     <?php if (have_comments()) : ?>
-      <ol class="commentlist list-unstyled">
+      <ol class="list-unstyled">
         <?php wp_list_comments(apply_filters('woocommerce_product_review_list_args', array('callback' => 'woocommerce_comments'))); ?>
       </ol>
 
