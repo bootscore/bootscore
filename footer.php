1a5ab67c94d77cf659aa1f68923a8dfaa93d7cc0
craftwerk <pst@crftwrk.de>
2025-01-17 12:36:56 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@ defined('ABSPATH') || exit;
 
 <?php do_action( 'bootscore_before_footer' ); ?>
 
-<footer class="bootscore-footer">
+<footer id="footer" class="bootscore-footer">
 
   <?php if (is_active_sidebar('footer-top')) : ?>
     <div class="<?= apply_filters('bootscore/class/footer/top', 'bg-body-tertiary border-bottom py-5'); ?> bootscore-footer-top">
