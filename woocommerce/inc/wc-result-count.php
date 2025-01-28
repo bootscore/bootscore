8efde5c61a63163ffc8ed9b45ef9d94022c55c32
craftwerk <pst@crftwrk.de>
2025-01-28 15:45:22 +0100

    Renamed functions

--- woocommerce/inc/wc-result-count.php
+++ woocommerce/inc/wc-result-count.php
@@ -13,34 +13,34 @@ defined('ABSPATH') || exit;
 
 
 /**
- * Add columns
+ * Add grid
  */
-add_action( 'woocommerce_before_shop_loop', 'add_opening_row_wrapper', 15 );
-function add_opening_row_wrapper() {
+add_action( 'woocommerce_before_shop_loop', 'bootscore_add_opening_row_wrapper', 15 );
+function bootscore_add_opening_row_wrapper() {
   echo '<div class="row mb-4">';
 }
 
-add_action( 'woocommerce_before_shop_loop', 'add_opening_result_count_col_wrapper', 18 );
-function add_opening_result_count_col_wrapper() {
+add_action( 'woocommerce_before_shop_loop', 'bootscore_add_opening_result_count_col_wrapper', 18 );
+function bootscore_add_opening_result_count_col_wrapper() {
   echo '<div class="col-md-6 col-lg-8 col-xxl-9">';
 }
 
-add_action( 'woocommerce_before_shop_loop', 'close_result_count_col_wrapper', 22 );
-function close_result_count_col_wrapper() {
+add_action( 'woocommerce_before_shop_loop', 'bootscore_close_result_count_col_wrapper', 22 );
+function bootscore_close_result_count_col_wrapper() {
   echo '</div>';
 }
 
-add_action( 'woocommerce_before_shop_loop', 'add_opening_orderby_col_wrapper', 29 );
-function add_opening_orderby_col_wrapper() {
+add_action( 'woocommerce_before_shop_loop', 'bootscore_add_opening_orderby_col_wrapper', 29 );
+function bootscore_add_opening_orderby_col_wrapper() {
   echo '<div class="col-md-6 col-lg-4 col-xxl-3">';
 }
 
-add_action( 'woocommerce_before_shop_loop', 'close_orderby_col_wrapper', 31 );
-function close_orderby_col_wrapper() {
+add_action( 'woocommerce_before_shop_loop', 'bootscore_close_orderby_col_wrapper', 31 );
+function bootscore_close_orderby_col_wrapper() {
   echo '</div>';
 }
 
-add_action( 'woocommerce_before_shop_loop', 'close_row_wrapper', 32 );
-function close_row_wrapper() {
+add_action( 'woocommerce_before_shop_loop', 'bootscore_close_row_wrapper', 32 );
+function bootscore_close_row_wrapper() {
   echo '</div>';
 }
