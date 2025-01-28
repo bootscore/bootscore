0000796464ec63932eb3ef876e1ef8f88f6372f3
Massimo Ghielmi <98320003+massimoghm@users.noreply.github.com>
2025-01-28 11:22:34 +0100

    Create tinymce-editor.php
    Fix for issues 442/934
    Pull 757
    
    Fix TinyMCE editor body margin and font-family that is overridden by Bootstrap editor-styles.

---
+++ inc/tinymce-editor.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * TinyMCE Editor
+ *
+ * @package Bootscore 
+ * @version 6.1.0
+ */
+
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+/*
+ * Fix TinyMCE editor body margin and font-family that is overridden by Bootstrap editor-styles
+ * https://github.com/bootscore/bootscore/issues/442
+ * https://github.com/bootscore/bootscore/pull/757
+ * https://github.com/bootscore/bootscore/issues/934
+ */
+add_filter( 'tiny_mce_before_init', function( $settings ) {
+
+  $content_style = 'body.mce-content-body { margin: 9px 10px; font-family: inherit; }';
+
+  if ( isset( $settings['content_style'] ) ) {
+    $settings['content_style'] .= ' ' . $content_style;
+  } else {
+    $settings['content_style'] = $content_style;
+  }
+
+  return $settings;
+});
