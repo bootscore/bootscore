fb29bcd15bf05d75b26ee53ef7a2c580d0447f7e
Massimo Ghielmi <98320003+massimoghm@users.noreply.github.com>
2025-01-28 11:07:05 +0100

    Update searchform.php
    Added aria-label for search input and search submit button.
    Fixed esc_attr_e for placeholder.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <!-- Search Button Outline Secondary Right -->
 <form class="searchform input-group" method="get" action="<?= esc_url(home_url('/')); ?>">
-  <input type="text" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">
-  <button type="submit" class="input-group-text btn btn-outline-secondary"><i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span></button>
+  <input type="text" name="s" class="form-control" placeholder="<?php esc_attr_e('Search', 'bootscore'); ?>" aria-label="<?php esc_attr_e( 'Search', 'bootscore' ); ?>">
+  <button type="submit" class="input-group-text btn btn-outline-secondary" aria-label="<?php esc_attr_e( 'Submit search', 'bootscore' ); ?>"><i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span></button>
 </form>
