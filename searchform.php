e0c27ee81e16a66ce65702354375348b3fb79046
craftwerk <pst@crftwrk.de>
2025-01-29 11:12:20 +0100

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -17,6 +17,6 @@ defined('ABSPATH') || exit;
 
 
 <form class="searchform input-group" method="get" action="<?= esc_url(home_url('/')); ?>">
-  <input type="text" name="s" class="form-control" placeholder="<?php _e('Search', 'bootscore'); ?>">
-  <button type="submit" class="input-group-text <?= apply_filters('bootscore/class/widget/search/button', 'btn btn-outline-secondary'); ?>" aria-label="<?php esc_attr_e( 'Submit search', 'bootscore' ); ?>"><?= apply_filters('bootscore/icon/search', '<i class="fa-solid fa-magnifying-glass"></i>'); ?> <span class="visually-hidden-focusable">Search</span></button>
+  <input type="text" name="s" class="form-control" placeholder="<?php esc_attr_e('Search', 'bootscore'); ?>" aria-label="<?php esc_attr_e( 'Search', 'bootscore' ); ?>">
+  <button type="submit" class="input-group-text btn btn-outline-secondary" aria-label="<?php esc_attr_e( 'Submit search', 'bootscore' ); ?>"><i class="fa-solid fa-magnifying-glass"></i><span class="visually-hidden-focusable">Search</span></button>
 </form>
