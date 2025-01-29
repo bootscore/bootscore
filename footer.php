1af68d13bc8ae7064d036b35cc0c920f35f28bb5
craftwerk <pst@crftwrk.de>
2025-01-29 11:02:17 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -93,7 +93,7 @@ defined('ABSPATH') || exit;
 </footer>
 
 <!-- To top button -->
-<a href="#" class="<?= apply_filters('bootscore/class/footer/to_top_button', 'btn btn-primary shadow'); ?> position-fixed zi-1000 top-button" aria-label="<?php esc_html_e('Return to top', 'bootscore' ); ?>"><?= apply_filters('bootscore/icon/chevron-up', '<i class="fa-solid fa-chevron-up"></i>'); ?><span class="visually-hidden-focusable">To top</span></a>
+<a href="#" class="<?= apply_filters('bootscore/class/footer/to_top_button', 'btn btn-primary shadow'); ?> position-fixed zi-1000 top-button" aria-label="<?php esc_attr_e('Return to top', 'bootscore' ); ?>"><?= apply_filters('bootscore/icon/chevron-up', '<i class="fa-solid fa-chevron-up"></i>'); ?><span class="visually-hidden-focusable">To top</span></a>
 
 </div><!-- #page -->
 
