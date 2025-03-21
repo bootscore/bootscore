2ca51fd172c38980b6c037ee0284eb36b729c051
craftwerk <pst@crftwrk.de>
2025-03-21 12:03:57 +0100

    Improve card patterns

---
+++ patterns/c.card-categories.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Title: c - card category widget
+ * Slug: bootscore/c.card-categories
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.1.2
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"metadata":{"name":"c - card category widget - card hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/card-categories"},"className":"card hide-wp-block-classes"} -->
+<div class="wp-block-group card hide-wp-block-classes"><!-- wp:heading {"metadata":{"name":"card-header h6"},"className":"card-header h6"} -->
+<h2 class="wp-block-heading card-header h6">Categories</h2>
+<!-- /wp:heading -->
+
+<!-- wp:categories {"showPostCounts":true,"className":"list-group-flush"} /--></div>
+<!-- /wp:group -->s
