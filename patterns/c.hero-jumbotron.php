5a5bc3f105141a396ad947ea2925878f46eaec49
craftwerk <pst@crftwrk.de>
2025-03-22 11:42:08 +0100

    Fix jumbotron

---
+++ patterns/c.hero-jumbotron.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: c - Hero jumbotron
+ * Slug: bootscore/c.hero-jumbotron
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.1.2
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"metadata":{"name":"c - Hero jumbotron - p-5 text-center bg-body-tertiary rounded-3 hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/hero-jumbotron"},"className":"p-5 text-center bg-body-tertiary rounded-3 hide-wp-block-classes","layout":{"type":"default"}} -->
+<div class="wp-block-group p-5 text-center bg-body-tertiary rounded-3 hide-wp-block-classes"><!-- wp:heading {"level":1,"metadata":{"name":"text-body-emphasis"},"className":"text-body-emphasis"} -->
+<h1 class="wp-block-heading text-body-emphasis">Basic jumbotron</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"metadata":{"name":"lead mb-0"},"className":"lead mb-0"} -->
+<p class="lead mb-0">This is a simple Bootstrap jumbotron, recreated with built-in utility classes.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
