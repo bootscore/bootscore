fa09b073de21c045f5da1d9e736975641998fff9
craftwerk <pst@crftwrk.de>
2025-03-22 11:35:47 +0100

    Fix container

---
+++ patterns/l.container.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: l - container
+ * Slug: bootscore/l.container
+ * Categories: bootscore
+ * https://developer.wordpress.org/themes/features/block-patterns/
+ * 
+ * @package Bootscore
+ * @version 6.1.2
+ */
+
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
+?>
+<!-- wp:group {"metadata":{"name":"l - container hide-wp-block-classes","categories":["bootscore"],"patternName":"bootscore/container"},"className":"container hide-wp-block-classes","layout":{"type":"default"}} -->
+<div class="wp-block-group container hide-wp-block-classes"><!-- wp:paragraph -->
+<p>This is a <code>container</code></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
