94da94ec18fa9f72d36d0c53ab517f9f0c5bf1a0
craftwerk <pst@crftwrk.de>
2025-03-30 15:19:18 +0200

    Fix unsupported blocks in widget area

--- inc/blocks/disable-unsupported-blocks.php
+++ inc/blocks/disable-unsupported-blocks.php
@@ -16,54 +16,64 @@ defined('ABSPATH') || exit;
  * Disable unsupported blocks
  */
 add_filter('allowed_block_types_all', function( $allowed_blocks, $editor_context ) {
+
+  $supported_blocks = [
+    // Text
+    'core/paragraph',
+    'core/heading',
+    'core/list',
+    'core/quote',
+    'core/code',
+    'core/preformatted',
+    'core/table',
+    'core/freeform',
+
+    // Media
+    'core/image',
+    'core/gallery',
+    'core/audio',
+    'core/video',
+
+    // Design
+    'core/buttons',
+    'core/group',
+    'core/separator',
+    'core/spacer',
+
+    // Widgets
+    'core/archives',
+    'core/calendar',
+    'core/categories',
+    'core/html',
+    'core/latest-comments',
+    'core/latest-posts',
+    'core/search',
+    'core/shortcode',
+
+    // Embeds
+    'core/embed',
+
+    // WooCommerce
+    'woocommerce/product-categories',
+    'woocommerce/classic-shortcode',
+    'woocommerce/cart',
+    'woocommerce/checkout',
+  ];
+
+  // Post editor
   if ( ! empty( $editor_context->post ) ) {
-    return [
-
-      // Text
-      'core/paragraph',
-      'core/heading',
-      'core/list',
-      'core/quote',
-      'core/code',
-      'core/preformatted',
-      'core/table',
-      'core/freeform',
-
-      // Media
-      'core/image',
-      'core/gallery',
-      'core/audio',
-      'core/video',
-      
-      // Design
-      'core/buttons',
-      'core/group',
-      'core/separator',
-      'core/spacer',
-      
-      // Widgets
-      'core/archives',
-      'core/calendar',
-      'core/categories',
-      'core/html',
-      'core/latest-comments',
-      'core/latest-posts',
-      'core/search',
-      'core/shortcode',
-      
-      // Embeds
-      'core/embed',
-      
-      // WooCommerce
-      'woocommerce/product-categories',
-      'woocommerce/classic-shortcode',
-      'woocommerce/cart',
-      'woocommerce/checkout',
-      
-    ];
+    return $supported_blocks;
   }
-    return $allowed_blocks;
+
+  // Block-based Widgets screen
+  if ( is_admin() && get_current_screen() && get_current_screen()->id === 'widgets' ) {
+    return $supported_blocks;
+  }
+
+  return $allowed_blocks;
 }, 10, 2);
+
+
 
 
 
