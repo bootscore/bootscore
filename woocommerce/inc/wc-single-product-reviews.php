e231a0cc64dc1c0eb9319730ee7142a7e9c53977
craftwerk <pst@crftwrk.de>
2025-09-05 11:26:04 +0200

    Added class form-control to name and mail forms if WC reviews are open to everyone

--- woocommerce/inc/wc-single-product-reviews.php
+++ woocommerce/inc/wc-single-product-reviews.php
@@ -4,7 +4,7 @@
  * WooCommerce Single Product Reviews
  *
  * @package Bootscore 
- * @version 6.1.0
+ * @version 6.3.1
  */
 
 
@@ -16,18 +16,12 @@
  * Replace <div id="comments"> with <div id="woo-comments"> in WooCommerce reviews.
  */
 add_action('woocommerce_after_single_product_summary', function () {
-  // Start output buffering.
   ob_start();
 }, 1);
 
 add_action('woocommerce_after_single_product_summary', function () {
-  // Get the buffered content.
   $output = ob_get_clean();
-
-  // Replace <div id="comments"> with <div id="woo-comments">.
   $output = str_replace('<div id="comments"', '<div id="woo-comments"', $output);
-
-  // Output the modified content.
   echo $output;
 }, 100);
 
@@ -37,11 +31,9 @@ add_action('woocommerce_after_single_product_summary', function () {
  */
 add_filter('woocommerce_product_review_comment_form_args', function ($args) {
   if (!empty($args['title_reply'])) {
-    // Replace the reply title span with h3
     $args['title_reply_before'] = '<h3 id="reply-title" class="comment-reply-title">';
-    $args['title_reply_after'] = '</h3>';
+    $args['title_reply_after']  = '</h3>';
   }
-
   return $args;
 });
 
@@ -50,40 +42,60 @@ add_filter('woocommerce_product_review_comment_form_args', function ($args) {
  * Replace <ol class="commentlist"> with <ul class="comment-list"> in WooCommerce reviews.
  */
 add_action('woocommerce_after_single_product_summary', function () {
-  // Start output buffering.
   ob_start();
 }, 1);
 
 add_action('woocommerce_after_single_product_summary', function () {
-  // Get the buffered content.
   $output = ob_get_clean();
-
-  // Replace <ol class="commentlist"> with <ul class="comment-list">.
   $output = str_replace('<ol class="commentlist">', '<ul class="comment-list">', $output);
-
-  // Replace the closing </ol> tag with </ul>.
   $output = str_replace('</ol>', '</ul>', $output);
-
-  // Output the modified content.
   echo $output;
 }, 100);
+
+
+/**
+ * Rating dropdown (Block themes)
+ */
+add_filter('woocommerce_product_review_comment_form_args', function ($comment_form) {
+  if (isset($comment_form['comment_field'])) {
+    $comment_form['comment_field'] = str_replace(
+      '<select',
+      '<select class="form-select mb-3"',
+      $comment_form['comment_field']
+    );
+  }
+  return $comment_form;
+});
 
 
 /**
  * Reviews text area
  */
 add_filter('comment_form_field_comment', function ($field) {
-  // Remove the label
   $field = preg_replace('/<label[^>]*for="comment"[^>]*>.*?<\/label>/i', '', $field);
-
-  // Add the 'form-control' class and placeholder to the textarea
   $field = str_replace(
     '<textarea',
     '<textarea class="form-control" placeholder="' . __('Your review...*', 'bootscore') . '"',
     $field
   );
-
   return $field;
+});
+
+
+/**
+ * Review name and email form fields
+ */
+add_filter('woocommerce_product_review_comment_form_args', function ($comment_form) {
+  foreach (['author', 'email'] as $field) {
+    if (isset($comment_form['fields'][$field])) {
+      $comment_form['fields'][$field] = str_replace(
+        '<input',
+        '<input class="form-control"',
+        $comment_form['fields'][$field]
+      );
+    }
+  }
+  return $comment_form;
 });
 
 
@@ -91,21 +103,15 @@ add_filter('comment_form_field_comment', function ($field) {
  * Add 'woocommerce-info' class to the verification required message for WooCommerce reviews.
  */
 add_action('woocommerce_after_single_product_summary', function () {
-  // Start output buffering.
   ob_start();
 }, 1);
 
 add_action('woocommerce_after_single_product_summary', function () {
-  // Get the buffered content.
   $output = ob_get_clean();
-
-  // Replace the verification required paragraph with the desired class.
   $output = str_replace(
     '<p class="woocommerce-verification-required">',
     '<p class="woocommerce-verification-required woocommerce-info">',
     $output
   );
-
-  // Output the modified content.
   echo $output;
 }, 100);
