9828e9547b2ce16f19212441eb7d9d7f6a73ee11
craftwerk <pst@crftwrk.de>
2025-09-05 11:35:01 +0200

    Modify WooCommerce reviews markup in one place

--- woocommerce/inc/wc-single-product-reviews.php
+++ woocommerce/inc/wc-single-product-reviews.php
@@ -13,7 +13,7 @@
 
 
 /**
- * Replace <div id="comments"> with <div id="woo-comments"> in WooCommerce reviews.
+ * Modify WooCommerce reviews markup in one place
  */
 add_action('woocommerce_after_single_product_summary', function () {
   ob_start();
@@ -21,97 +21,93 @@ add_action('woocommerce_after_single_product_summary', function () {
 
 add_action('woocommerce_after_single_product_summary', function () {
   $output = ob_get_clean();
-  $output = str_replace('<div id="comments"', '<div id="woo-comments"', $output);
-  echo $output;
+
+  if ($output) {
+    // 1. Replace <div id="comments"> with <div id="woo-comments">
+    $output = str_replace('<div id="comments"', '<div id="woo-comments"', $output);
+
+    // 2. Replace <ol class="commentlist"> with <ul class="comment-list">
+    $output = str_replace('<ol class="commentlist">', '<ul class="comment-list">', $output);
+    $output = str_replace('</ol>', '</ul>', $output);
+
+    // 3. Add 'woocommerce-info' class to the verification required message
+    $output = str_replace(
+      '<p class="woocommerce-verification-required">',
+      '<p class="woocommerce-verification-required woocommerce-info">',
+      $output
+    );
+
+    echo $output;
+  }
 }, 100);
 
 
 /**
- * Change reply title to h3 in WooCommerce reviews
+ * Customize WooCommerce review form fields
  */
 add_filter('woocommerce_product_review_comment_form_args', function ($args) {
+
+  // Change reply title to h3
   if (!empty($args['title_reply'])) {
     $args['title_reply_before'] = '<h3 id="reply-title" class="comment-reply-title">';
     $args['title_reply_after']  = '</h3>';
   }
+
+  // Style name and email fields
+  foreach (['author', 'email'] as $field) {
+    if (isset($args['fields'][$field])) {
+      // Add form-label class to the label
+      $args['fields'][$field] = str_replace(
+        '<label',
+        '<label class="form-label"',
+        $args['fields'][$field]
+      );
+
+      // Add form-control class to the input
+      $args['fields'][$field] = str_replace(
+        '<input',
+        '<input class="form-control"',
+        $args['fields'][$field]
+      );
+    }
+  }
+
+  // Style rating select (if ratings enabled)
+  if (isset($args['comment_field'])) {
+    $args['comment_field'] = str_replace(
+      '<select',
+      '<select class="form-select mb-3"',
+      $args['comment_field']
+    );
+
+    $args['comment_field'] = str_replace(
+      '<label',
+      '<label class="form-label"',
+      $args['comment_field']
+    );
+  }
+
   return $args;
 });
 
 
 /**
- * Replace <ol class="commentlist"> with <ul class="comment-list"> in WooCommerce reviews.
- */
-add_action('woocommerce_after_single_product_summary', function () {
-  ob_start();
-}, 1);
-
-add_action('woocommerce_after_single_product_summary', function () {
-  $output = ob_get_clean();
-  $output = str_replace('<ol class="commentlist">', '<ul class="comment-list">', $output);
-  $output = str_replace('</ol>', '</ul>', $output);
-  echo $output;
-}, 100);
-
-
-/**
- * Rating dropdown (Block themes)
- */
-add_filter('woocommerce_product_review_comment_form_args', function ($comment_form) {
-  if (isset($comment_form['comment_field'])) {
-    $comment_form['comment_field'] = str_replace(
-      '<select',
-      '<select class="form-select mb-3"',
-      $comment_form['comment_field']
-    );
-  }
-  return $comment_form;
-});
-
-
-/**
- * Reviews text area
+ * Customize WooCommerce review textarea
  */
 add_filter('comment_form_field_comment', function ($field) {
-  $field = preg_replace('/<label[^>]*for="comment"[^>]*>.*?<\/label>/i', '', $field);
+  // Add form-label class to label
   $field = str_replace(
-    '<textarea',
-    '<textarea class="form-control" placeholder="' . __('Your review...*', 'bootscore') . '"',
+    '<label',
+    '<label class="form-label"',
     $field
   );
+
+  // Add Bootstrap form-control + placeholder to textarea
+  $field = str_replace(
+    '<textarea',
+    '<textarea class="form-control" placeholder="' . esc_attr__('Your review...*', 'bootscore') . '"',
+    $field
+  );
+
   return $field;
 });
-
-
-/**
- * Review name and email form fields
- */
-add_filter('woocommerce_product_review_comment_form_args', function ($comment_form) {
-  foreach (['author', 'email'] as $field) {
-    if (isset($comment_form['fields'][$field])) {
-      $comment_form['fields'][$field] = str_replace(
-        '<input',
-        '<input class="form-control"',
-        $comment_form['fields'][$field]
-      );
-    }
-  }
-  return $comment_form;
-});
-
-
-/**
- * Add 'woocommerce-info' class to the verification required message for WooCommerce reviews.
- */
-add_action('woocommerce_after_single_product_summary', function () {
-  ob_start();
-}, 1);
-
-add_action('woocommerce_after_single_product_summary', function () {
-  $output = ob_get_clean();
-  $output = str_replace(
-    '<p class="woocommerce-verification-required">',
-    '<p class="woocommerce-verification-required woocommerce-info">',
-    $output
-  );
-  echo $output;
-}, 100);
