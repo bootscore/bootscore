585d997d305bed529f60ce244cf9178925f07d1e
Basti <51531217+crftwrk@users.noreply.github.com>
2025-11-27 11:41:15 +0100

    Refactor class replacements in product filters block

--- woocommerce/inc/blocks/wc-block-widget-product-filters.php
+++ woocommerce/inc/blocks/wc-block-widget-product-filters.php
@@ -25,42 +25,74 @@ if (!function_exists('bootscore_wc_block_widget_product_filter_classes')) {
    */
   function bootscore_wc_block_widget_product_filter_classes($block_content, $block) {
     
+
+    
+    
     // Auxiliary function for class replacement
     $replace_class = function($html, $search, $replace) {
-        return preg_replace_callback('/class="([^"]+)"/', function($matches) use ($search, $replace) {
-            $classes = explode(' ', $matches[1]);
-            foreach ($classes as &$c) {
-                if ($c === $search) {
-                    $c = $replace; // substitui a classe exata
-                }
-            }
-            return 'class="' . implode(' ', $classes) . '"';
-        }, $html);
+      return preg_replace_callback('/class="([^"]+)"/', function($matches) use ($search, $replace) {
+        $classes = explode(' ', $matches[1]);
+        foreach ($classes as &$c) {
+          if ($c === $search) {
+            $c = $replace; // substitui a classe exata
+          }
+        }
+        return 'class="' . implode(' ', $classes) . '"';
+      }, $html);
     };
 
     // Replacement association
     $class_replacements = [
-        // Chips
-        'wc-block-product-filter-removable-chips__items' => 'list-unstyled d-flex flex-wrap gap-2',
-        'wc-block-product-filter-removable-chips__item' => 'badge bg-light-subtle border border-light-subtle text-light-emphasis rounded-pill text-decoration-none d-flex align-items-center',
-        'wc-block-product-filter-removable-chips__remove' => 'btn-close ms-1',
-        'wc-block-product-filter-removable-chips__remove-icon' => 'btn-close-icon d-none',
+      
+      // Wrapper
+      'wp-block-woocommerce-product-filters' => 'wp-block-woocommerce-product-filters hide-wp-block-classes hide-wp-block-inline-styles',
+      
+      // Mobile stuff
+      'wc-block-product-filters__open-overlay' => 'd-none',   // Open button
+      'wc-block-product-filters__overlay-header' => 'd-none', // Close button
+      'wc-block-product-filters__overlay' => 'test',          // Overlay
+      'wc-block-product-filters__overlay-wrapper' => 'test',  // Overlay wrapper
+      'wc-block-product-filters__overlay-dialog' => 'test',   // Overlay modal
+      'wc-block-product-filters__overlay-content' => 'test',  // Overlay content
+      'wc-block-product-filters__overlay-footer' => 'd-none', // Apply button
+      
 
-        // Clear filters button
-        'wp-block-button__link' => '',
-        'btn-outline-primary' => 'btn-sm btn-outline-danger w-100',
+      // Chips
+      'wc-block-product-filter-removable-chips__items' => 'list-unstyled d-flex flex-wrap gap-2',
+      'wc-block-product-filter-removable-chips__item' => 'badge bg-primary-subtle text-primary-emphasis text-decoration-none d-flex align-items-center',
+      'wc-block-product-filter-removable-chips__remove' => 'btn-close ms-1',
+      'wc-block-product-filter-removable-chips__remove-icon' => 'btn-close-icon d-none',
 
-        // Checks
-        'wc-block-product-filter-checkbox-list__item' => 'form-check',
-        'wc-block-product-filter-checkbox-list__label' => 'form-check-label',
-        'wc-block-product-filter-checkbox-list__input-wrapper' => '',
-        'wc-block-product-filter-checkbox-list__input' => 'form-check-input',
-        'wc-block-product-filter-checkbox-list__text-wrapper' => '',
+      // Clear filters button
+      'wp-block-button__link' => '',
+      'btn-outline-primary' => 'btn btn-link p-0',
+      
+      // Price range
+      //'text' => 's',
+      //'min' => 'min form-control',
+      
+
+      // Checks
+      'wc-block-product-filter-checkbox-list__item' => 'form-check',
+      'wc-block-product-filter-checkbox-list__label' => 'form-check-label d-block cursor-pointer',
+      'wc-block-product-filter-checkbox-list__input-wrapper' => '',
+      'wc-block-product-filter-checkbox-list__input' => 'form-check-input',
+      'wc-block-product-filter-checkbox-list__text-wrapper' => 'd-flex align-items-center',
+      
+      'wc-block-product-filter-checkbox-list__count' => 'badge bg-primary-subtle text-primary-emphasis ms-auto',
+      
     ];
 
     foreach ($class_replacements as $search => $replace) {
         $block_content = $replace_class($block_content, $search, $replace);
     }
+    
+    // Remove parentheses inside Bootstrap-styled count spans
+    $block_content = preg_replace(
+      '/(<span[^>]*badge[^>]*>)[\s()]*([0-9]+)[\s()]*(<\/span>)/',
+      '$1$2$3',
+      $block_content
+    );
 
     return apply_filters('bootscore/block/product/filters/content', $block_content, $block);
   }
