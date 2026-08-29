b1c1a2e608b2ea560518918471b6149ddbe19e7e
Basti <51531217+crftwrk@users.noreply.github.com>
2026-08-29 10:50:11 +0200

    v7: Remove first batch of deprecated code

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,8 +1,9 @@
 /*--------------------------------------------------------------
-Theme JS - Bootscore v6.4.0
+Theme JS - Bootscore v7.0.0
 --------------------------------------------------------------*/
 
 jQuery(function ($) {
+  // Remove this and add an example to scrollspy docs
   // Close offcanvas on click a, keep .dropdown-menu open (see https://github.com/bootscore/bootscore/discussions/347)
   $('.offcanvas a:not(.dropdown-toggle, .remove_from_cart_button)').on('click', function () {
     $('.offcanvas').offcanvas('hide');
@@ -30,12 +31,5 @@ jQuery(function ($) {
       $('.top-button').removeClass('visible');
     }
   });
-
-  // div height, add class to your content
-  // Deprecated, this code will be removed in v7. CSS viewport units are much better.
-  $('.height-50').css('height', 0.5 * $(window).height());
-  $('.height-75').css('height', 0.75 * $(window).height());
-  $('.height-85').css('height', 0.85 * $(window).height());
-  $('.height-100').css('height', 1.0 * $(window).height());
 
 }); // jQuery End
